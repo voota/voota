7f33a2e77a1c892e31c20fa6639e0b576f962596
(no author) <(no author)@f81fdc05-8907-49aa-9672-817e67ad6f7d>
2009-08-05 16:18:08 +0000

    git-svn-id: http://trac.voota.org/svn/voota/trunk@8 f81fdc05-8907-49aa-9672-817e67ad6f7d

---
+++ www/web/js/voota.js
@@ -0,0 +1,12 @@
+function thumb_up() {
+	document.theForm.v[0].checked = true;
+	document.theForm.submit();
+	
+	return false;
+}
+function thumb_down() {
+	document.theForm.v[1].checked = true;
+	document.theForm.submit();
+	
+	return false;
+}
