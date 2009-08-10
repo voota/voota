1d7fee8ede2f8565395d23f9cf231f1397acdc1a
(no author) <(no author)@f81fdc05-8907-49aa-9672-817e67ad6f7d>
2009-08-10 16:08:23 +0000

    git-svn-id: http://trac.voota.org/svn/voota/trunk@18 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,40 +1,48 @@
 #!/bin/sh
 
 URL=http://trac.voota.org/svn/voota
-DEST=/tmp/voota
+DEST=/var/www/voota
 TEST=1
 PROD=2
-ENV=$TEST
+ENV=$PROD
 
 cd $DEST
 if [ $ENV -eq $TEST ] ; then
+  if [ $# -eq 0 ] ; then
+        echo "Usage: deploy.sh update|<release>"
+        echo "deploy.sh release [<release>]"
+        exit 1
+  fi
   case "$1" in
     release)
-	release=`date +%Y%m%d`
-	if [ $# -eq 2 ] ; then
-		release=$2
-	fi
-	svn copy -m $release $URL/trunk $URL/branches/$release
-	svn switch $URL/branches/$release
-	echo "Created release $release"
-	;;
-    *)
-	svn update
-	;;
+        release=`date +%Y%m%d`
+        if [ $# -eq 2 ] ; then
+                release=$2
+        fi
+        svn copy -m $release $URL/trunk $URL/branches/$release
+        svn switch $URL/branches/$release
+        echo "Created release $release"
+        ;;
+    update)
+        svn update
+        ;;
   esac
 fi
 if [ $ENV -eq $PROD ] ; then
   if [ $# -eq 0 ] ; then
-	echo "Usage: deploy.sh <release>"
-	exit 1
+        echo "Usage: deploy.sh update|<release>"
+        exit 1
   fi
   case "$1" in
     update)
-	svn update;
-	;;
+        svn update;
+        ;;
     *)
-  	release=$1
-  	svn switch $URL/branches/$release
-	;;
+        release=$1
+        svn switch $URL/branches/$release
+        ;;
   esac
 fi
+
+cd $DEST/www
+php symfony cc
