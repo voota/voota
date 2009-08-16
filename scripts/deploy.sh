2b21298f1e8caac59a639f2595bc90dd999c41b6
Sergio Viteri <sergio@infovit.net>
2009-08-16 13:58:13 +0000

    git-svn-id: http://trac.voota.org/svn/voota/trunk@54 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,49 +1,23 @@
 #!/bin/sh
 
-URL=http://trac.voota.org/svn/voota
 DEST=/var/www/voota
-TEST=1
-PROD=2
-ENV=$TEST
+DEST=/var/www/voota_admin
 
-cd $DEST
 if [ $ENV -eq $TEST ] ; then
   if [ $# -eq 0 ] ; then
-        echo "Usage: deploy.sh update|<release>"
-        echo "deploy.sh release [<release>]"
+        echo "Usage: deploy.sh <env>"
         exit 1
   fi
   case "$1" in
-    release)
-        release=`date +%Y%m%d`
-        if [ $# -eq 2 ] ; then
-                release=$2
-        fi
-        svn copy -m $release $URL/trunk $URL/branches/$release
-        svn switch $URL/branches/$release
-        echo "Created release $release"
+    test)
+		cd $DEST
+        phing update
         ;;
-    update)
-        svn update
+    prod)
+		cd $DEST
+        phing prod
+		cd $DEST_ADMIN
+        phing admin
         ;;
   esac
 fi
-if [ $ENV -eq $PROD ] ; then
-  if [ $# -eq 0 ] ; then
-        echo "Usage: deploy.sh update|<release>"
-        exit 1
-  fi
-  case "$1" in
-    update)
-        svn update;
-        ;;
-    *)
-        release=$1
-        svn switch $URL/branches/$release
-        ;;
-  esac
-fi
-
-cd $DEST/www
-chmod -R 777 $DEST/metadata/pootle
-php symfony cc
