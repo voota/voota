4f8c3e88e933bdfb0fe59d7bc15c63a329b0f2d5
Sergio Viteri <sergio@infovit.net>
2009-09-29 10:22:33 +0000

    git-svn-id: http://trac.voota.org/svn/voota/trunk@110 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- www/web/js/voota.js
+++ www/web/js/voota.js
@@ -10,3 +10,33 @@ function thumb_down() {
 	
 	return false;
 }
+function changeParam(param, value){
+	var separator = "?";
+	var dest = "" + document.location; 
+	if(dest.indexOf("?") != -1){
+		separator = "&";
+	}
+	document.location = document.location + separator + param + '=' + value;
+	
+	return false;
+}
+function rankingReady() {	
+	$("#partido_selector").change(function(){
+		partidoSelector = document.getElementById('partido_selector');
+		
+		changeParam("p", partidoSelector.value);
+	});}
+
+function politicoReady( id ){
+	$(".yeah").click(function(){
+		loadReviewBox(1, id, 1);
+	});
+	$(".buu").click(function(){
+		loadReviewBox(1, id, -1);
+	});
+}
+
+
+function changeInstitucion( institucion ){
+	return changeParam("i", institucion);
+}
