701c45163ef1b4981fdc1a7f40387bca27a89384
Sergio Viteri <sergio@infovit.net>
2009-10-16 16:38:34 +0000

    git-svn-id: http://trac.voota.org/svn/voota/trunk@151 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- www/web/js/voota.js
+++ www/web/js/voota.js
@@ -36,6 +36,11 @@ function politicoReady( id ){
 	});
 }
 
+function loadReviewBox(t, e, v) {
+
+	jQuery.ajax({type:'POST',dataType:'html',success:function(data, textStatus){jQuery('#sf_review').html(data);},url:'/review/form?t='+t+'&e='+e+'&v='+v+''}); return false;
+}
+
 
 function changeInstitucion( institucion ){
 	return changeParam("i", institucion);
