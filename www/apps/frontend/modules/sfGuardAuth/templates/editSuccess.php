e38ebe6a238d40c95ea47495f9d233c7f1ab60db
Carlos Paramio <carlos@evolve.st>
2009-11-26 17:34:04 +0000

    revisión de página de editar perfil
    git-svn-id: http://trac.voota.org/svn/voota/trunk@302 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- www/apps/frontend/modules/sfGuardAuth/templates/editSuccess.php
+++ www/apps/frontend/modules/sfGuardAuth/templates/editSuccess.php
@@ -20,7 +20,7 @@
 <div class="limpiar"></div>
 <div class="formSing">
 <?php echo form_tag('@usuario_edit', 'multipart=true autocomplete=off') ?>
-<table>   
+<table id="editProfile">   
   <tr>
     <td class="anchoColumna tituloCampo tdperfil">
       <?php if ($profileEditForm['nombre']->hasError()): ?><ul class="error_list"><li>&nbsp;</li></ul><?php endif ?>
@@ -35,34 +35,37 @@
   <tr>
     <td class="anchoColumna tituloCampo tdperfil">
       <?php if ($profileEditForm['apellidos']->hasError()): ?><ul class="error_list"><li>&nbsp;</li></ul><?php endif ?>
-      <?php echo __('Tus apellidos') ?> <?php echo __('(opcional)') ?>
+      <?php echo __('Tus apellidos') ?>
     </td>
     <td class="wider tdperfil" >
       <?php echo $profileEditForm['apellidos']->renderError() ?>
       <?php echo $profileEditForm['apellidos']->render() ?>
-    </td>    
+    </td>
+    <td class="tdperfil tips"><?php echo __('(opcional)') ?></td>
   </tr>
    
   <tr>
     <td class="anchoColumna tituloCampo tdperfil">
       <?php if ($profileEditForm['presentacion']->hasError()): ?><ul class="error_list"><li>&nbsp;</li></ul><?php endif ?>
-      <?php echo __('Presentación') ?> <?php echo __('(opcional)') ?>
+      <?php echo __('Presentación') ?>
     </td>
     <td class="wider tdperfil" >
       <?php echo $profileEditForm['presentacion']->renderError() ?>
-      <?php echo $profileEditForm['presentacion']->render() ?>
-    </td>    
+      <?php echo $profileEditForm['presentacion']->render('presentacion', null, array('rows' => 15)) ?>
+    </td>
+    <td class="tdperfil tips"><?php echo __('(opcional)') ?></td>
   </tr>
 
   <tr>
     <td class="anchoColumna tituloCampo tdperfil">
       <?php if ($profileEditForm['fecha_nacimiento']->hasError()): ?><ul class="error_list"><li>&nbsp;</li></ul><?php endif ?>
-      <?php echo __('Fecha de nacimiento') ?> <?php echo __('(opcional)') ?>
+      <?php echo __('Fecha de nacimiento') ?>
     </td>
     <td class="wider tdperfil" >
       <?php echo $profileEditForm['fecha_nacimiento']->renderError() ?>
       <?php echo $profileEditForm['fecha_nacimiento']->render() ?>
-    </td>    
+    </td>
+    <td class="tdperfil tips"><?php echo __('(opcional)') ?></td>
   </tr>
   
   <tr>
@@ -72,19 +75,20 @@
     </td>
     <td class="wider tdperfil" >
       <?php echo $profileEditForm['vanity']->renderError() ?>
-      <span class="nombrePeque"><?php echo __('http://voota.es/') ?></span><?php echo $profileEditForm['vanity']->render() ?>
+      <span class="nombrePeque"><?php echo __('http://voota.es/') ?></span><?php echo $profileEditForm['vanity']->render('vanity', null, array('class' => 'inputSignVanity')) ?>
     </td>    
   </tr>
   
   <tr>
     <td class="anchoColumna tituloCampo tdperfil">
       <?php if ($profileEditForm['imagen']->hasError()): ?><ul class="error_list"><li>&nbsp;</li></ul><?php endif ?>
-      <?php echo __('Tu avatar') ?> <?php echo __('(opcional)') ?>
+      <?php echo __('Tu avatar') ?>
     </td>
     <td class="wider tdperfil" >
       <?php echo $profileEditForm['imagen']->renderError() ?>
       <?php echo $profileEditForm['imagen']->render() ?>
-    </td>    
+    </td>
+    <td class="tdperfil tips"><?php echo __('(opcional)') ?></td>
   </tr>
    
   <tr>
@@ -101,12 +105,13 @@
   <tr>
     <td class="anchoColumna tituloCampo tdperfil">
       <?php if ($profileEditForm['passwordNew']->hasError()): ?><ul class="error_list"><li>&nbsp;</li></ul><?php endif ?>
-      <?php echo __('¿Nueva contraseña?') ?> <?php echo __('(opcional)') ?>
+      <?php echo __('¿Nueva contraseña?') ?>
     </td>
-    <td class="leftSing tdperfil" >
+    <td class="tdperfil" >
       <?php echo $profileEditForm['passwordNew']->renderError() ?>
       <?php echo $profileEditForm['passwordNew']->render() ?>
-    </td>    
+    </td>
+    <td class="tdperfil tips"><?php echo __('(opcional)') ?></td>
   </tr>
    
   <tr>
@@ -114,7 +119,7 @@
       <?php if ($profileEditForm['passwordBis']->hasError()): ?><ul class="error_list"><li>&nbsp;</li></ul><?php endif ?>
       <?php echo __('Repítela nuevamente') ?>
     </td>
-    <td class="leftSing tdperfil" >
+    <td class="tdperfil" >
       <?php echo $profileEditForm['passwordBis']->renderError() ?>
       <?php echo $profileEditForm['passwordBis']->render() ?>
     </td>    
@@ -125,7 +130,7 @@
       <?php if ($profileEditForm['passwordOld']->hasError()): ?><ul class="error_list"><li>&nbsp;</li></ul><?php endif ?>
       <?php echo __('Contraseña actual') ?>
     </td>
-    <td class="leftSing tdperfil" >
+    <td class="tdperfil" >
       <?php echo $profileEditForm['passwordOld']->renderError() ?>
       <?php echo $profileEditForm['passwordOld']->render() ?>
     </td>    
@@ -135,7 +140,7 @@
 
   <tr>
     <td>&nbsp;</td>
-    <td class="alingBoton">
+    <td class="tdperfil">
     <?php echo submit_tag(__('Guardar cambios'), array('class'  => 'button')) ?>
     
     </td>
