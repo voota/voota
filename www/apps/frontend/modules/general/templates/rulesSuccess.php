864544d260d83cc7b2e545b40229fd4a3f675ad1
Carlos Paramio <carlos@evolve.st>
2009-11-27 11:12:58 +0000

    cambios a página de normas de publicación
    git-svn-id: http://trac.voota.org/svn/voota/trunk@309 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- www/apps/frontend/modules/general/templates/rulesSuccess.php
+++ www/apps/frontend/modules/general/templates/rulesSuccess.php
@@ -11,7 +11,7 @@
 <div class="limpiar"></div>
 <div class="formSings">
 <h5><?php echo __('En pocas palabras ...') ?></h5>
-<h5><?php echo __('Las opiniones son libres, publica todo lo que quieras, a favor o en contra, salvo lo que esté prohibido por ley.') ?></h5>
+<h5><?php echo __('Las opiniones son libres, publica todo lo que quieras, a favor o en contra, salvo lo que está prohibido por ley.') ?></h5>
 <h5>&nbsp;</h5>
 </div>
 
@@ -24,16 +24,16 @@
 <li><h6>Insultar o amenazar.</h6></li>
 <li><h6>Incitar al odio, la violencia o la difamación.</h6></li>
 <li><h6>Incitar a la discriminación por razones de sexo, raza, religión, afiliación, política, creencias, edad o condición.</h6></li>
-<li><h6>Enlazar con páginas con contenidos ilegales.</h6></li>
+<li><h6>Enlazar a páginas con contenidos ilegales.</h6></li>
 <li><h6>Incluir contenidos protegidos por derechos de propiedad de terceros sin autorización.</h6></li>
 </ul>
-<h6>Más detalles?   <?php 
+<h6>¿Más detalles?   <?php 
   	echo link_to(
  	__('Date una vuelta por nuestro aviso legal')
  	, __('http://blog.voota.es/es/aviso-legal')
  	, array()
  ) ?></h6>
-<h6><strong>Ayudanos a mejorar Voota:</strong></h6>
+<h6><strong>Ayúdanos a mejorar Voota:</strong> <a href="http://otrs.voota.es/otrs/index.pl">Contáctanos si ves algún contenido ilegal</a></h6>
 
 </div>
 
