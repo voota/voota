5ebf0bf387b3ea1d19cc71cca0a6421f466741d9
Sergio Viteri <sergio@infovit.net>
2009-11-30 12:26:19 +0000

    git-svn-id: http://trac.voota.org/svn/voota/trunk@318 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- www/apps/frontend/templates/layout.php
+++ www/apps/frontend/templates/layout.php
@@ -1,14 +1,14 @@
 <?php use_helper('I18N') ?>
 <?php use_helper('Form') ?>
-<?xml version='1.0' encoding='utf-8' ?>
+<?php echo "<?xml version='1.0' encoding='utf-8' ?>" ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns='http://www.w3.org/1999/xhtml'>
 <head>
   <?php include_title() ?>
   <?php include_http_metas() ?>
   <?php include_metas() ?>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <link rel="shortcut icon" href="/favicon.ico" >
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <link rel="shortcut icon" href="/favicon.ico" />
 </head>
 
 <body id="<?php echo $sf_context->getModuleName()."-". $sf_context->getActionName() ?>"> 
