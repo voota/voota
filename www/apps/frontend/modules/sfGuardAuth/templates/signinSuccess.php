586a95dc0372ab8fbf95f730e924823e6f8f0d8e
Carlos Paramio <carlos@evolve.st>
2009-12-02 10:35:08 +0000

    arreglar página de login (la columna derecha se caía)
    git-svn-id: http://trac.voota.org/svn/voota/trunk@330 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- www/apps/frontend/modules/sfGuardAuth/templates/signinSuccess.php
+++ www/apps/frontend/modules/sfGuardAuth/templates/signinSuccess.php
@@ -6,162 +6,122 @@
 
 <?php echo showNotices( $sf_user ) ?>
 
-<!-- CONTENT LEFT-->
 <div id="contentLeftSing">
-<div title="ficha">
-<span class="tituloAzul"><?php echo __('¿Nuevo en Voota? Empieza aquí')?></span>
-<div class="limpiar"></div>
-<!--tu email -->
-<div class="formSing">
-<?php echo form_tag('@sf_guard_signin') ?>
-	<?php echo input_hidden_tag('op', 'r') ?>
+  <div title="ficha">
+    <span class="tituloAzul"><?php echo __('¿Nuevo en Voota? Empieza aquí')?></span>
+    <div class="limpiar"></div>
 
-<table id="signin">
-  <tr>
-      <td class="leftSing"><h5><?php echo __('Tu email') ?></h5></td>
-    <td class="campo">
-      <?php echo $registrationform['username']->render() ?>
-      <?php echo $registrationform['username']->renderError() ?>
-    </td>    
-    <td>&nbsp;</td>
-  </tr>
-  <tr>
-      <td class="leftSing"><h5>
-      <?php echo __('Nombre') ?>
-    </h5></td>
-    <td class="campo">
-      <?php echo $registrationform['nombre']->render() ?>
-      <?php echo $registrationform['nombre']->renderError() ?>
-    </td>    
-    <td>&nbsp;</td>
-  </tr>
-  <tr>
-      <td class="leftSing"><h5>
-      <?php echo __('Apellidos') ?>
-    </h5></td>
-    <td class="campo">
-      <?php echo $registrationform['apellidos']->render() ?>
-      <?php echo $registrationform['apellidos']->renderError() ?>
-    </td>    
-    <td>&nbsp;</td>
-  </tr>
-  <tr>
-      <td class="leftSing"><h5>
-      <?php echo __('Contraseña') ?>
-    </h5></td>
-    <td class="campo">
-      <?php echo $registrationform['password']->render() ?>
-      <?php echo $registrationform['password']->renderError() ?>
-    </td>    
-    <td>&nbsp;</td>
-  </tr>
+    <div class="formSing">
+      <?php echo form_tag('@sf_guard_signin') ?>
+	      <?php echo input_hidden_tag('op', 'r') ?>
 
+        <table id="signin">
+          <tr>
+            <td class="leftSing"><h5><?php echo __('Tu email') ?></h5></td>
+            <td class="campo">
+              <?php echo $registrationform['username']->render() ?>
+              <?php echo $registrationform['username']->renderError() ?>
+            </td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td class="leftSing"><h5>
+              <?php echo __('Nombre') ?>
+            </h5></td>
+            <td class="campo">
+              <?php echo $registrationform['nombre']->render() ?>
+              <?php echo $registrationform['nombre']->renderError() ?>
+            </td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td class="leftSing"><h5><?php echo __('Apellidos') ?></h5></td>
+            <td class="campo">
+              <?php echo $registrationform['apellidos']->render() ?>
+              <?php echo $registrationform['apellidos']->renderError() ?>
+            </td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td class="leftSing"><h5><?php echo __('Contraseña') ?></h5></td>
+            <td class="campo">
+              <?php echo $registrationform['password']->render() ?>
+              <?php echo $registrationform['password']->renderError() ?>
+            </td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td>
+              <h6>
+                <span id="show_pass_label"><a href="#" onclick="return showHidePass('registration_password')"><?php echo __('Ver la contraseña')?></a></span>
+    		        <span id="hide_pass_label" class="hidden"><a href="#" onclick="return showHidePass('registration_password')"><?php echo __('Ocultar la contraseña')?></a></span>
+    	        </h6>
+    	      </td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td class="leftSing"></td>
+            <td class="legal">
+              He leído y acepto el <?php echo link_to(__('aviso legal'), __('http://blog.voota.es/es/aviso-legal'), array()) ?><?php echo $registrationform['accept']->render() ?>
+              <?php echo $registrationform['accept']->renderError() ?>
+            </td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td class="leftSing">&nbsp;</td>
+            <td class="boton">
+              <?php echo submit_tag(__('Registrate en Voota'), array('class'   => 'button',)) ?>
+            </td>
+            <td>&nbsp;</td>
+          </tr>
+        </table>
+      </form>
+    </div>
+    <div class="limpiar"></div>
+  </div>
+</div><!-- FIN CONTENT LEFT -->
 
-  <tr>
-    <td>&nbsp;</td>
-    <td>
-    	<h6>
-    		<span id="show_pass_label"><a href="#" onclick="return showHidePass('registration_password')"><?php echo __('Ver la contraseña')?></a></span>
-    		<span id="hide_pass_label" class="hidden"><a href="#" onclick="return showHidePass('registration_password')"><?php echo __('Ocultar la contraseña')?></a></span>
-    	</h6>
-    	</td>
-    <td>&nbsp;</td>
-
-  </tr>
-  
-  <tr>
-    <td class="leftSing"></td>
-    <td class="legal">He leído y acepto el <?php echo link_to(__('aviso legal'), __('http://blog.voota.es/es/aviso-legal'), array()) ?><?php echo $registrationform['accept']->render() ?>
-    <?php echo $registrationform['accept']->renderError() ?></td>    
-    <td>&nbsp;</td>
-  </tr>
-  
-  
-  <tr>
-    <td class="leftSing">&nbsp;</td>
-    <td class="boton">
-      <?php echo submit_tag(__('Registrate en Voota'), array('class'   => 'button',)) ?>
-      </td>
-    <td>&nbsp;</td>
-  </tr>
-</table>
-</form>
-</div>
-
-<div class="limpiar"></div>
-<!--usuario -->
-<div class="limpiar"></div>
-<!--contraseña -->
-</div>
-</div>
-</div>
-<!-- FIN CONTENT LEFT -->
 <!-- CONTENT RIGHT -->
 <div id="contentRightSing">
-  <div title="ficha"> <span class="tituloAzul"><?php echo __('¿Ya estás registrado? Adelante :)')?></span>
-  
+  <div title="ficha">
+    <span class="tituloAzul"><?php echo __('¿Ya estás registrado? Adelante :)')?></span>
     <div class="formSing">
-<form action="<?php echo url_for('@sf_guard_signin') ?>" method="post">
-<table id="login">
-  <tr>
-      <td class="leftSing"><h5>
-      <?php echo __('Tu Email') ?>
-    </h5></td>
-    <td>
-      <?php echo $signinform['username']->renderError() ?>
-      <?php echo $signinform['username']->render() ?>
-    </td>    
-  </tr>
-  <tr>
-      <td class="leftSing"><h5>
-      <?php echo __('Contraseña') ?>
-    </h5></td>
-    <td>
-      <?php echo $signinform['password']->renderError() ?>
-      <?php echo $signinform['password']->render() ?>
-    </td>    
-  </tr>
-  <tr>
-      <td class="leftSing"><h5>
-      <?php echo __('Recordar') ?>
-    </h5></td>
-    <td>
-      <?php echo $signinform['remember']->renderError() ?>
-      <?php echo $signinform['remember']->render() ?>
-    </td>    
-  </tr>
-
-
-  <tr>
-    <td>&nbsp;</td>
-    <td><h6>
-
- <?php echo link_to(
- 	__('¿Olvidaste tu contraseña?')
- 	, 'sfGuardAuth/reminder'
- ) ?>
-      
-    </h6></td>
-    </tr>
-  <tr>
-    <td class="leftSing">&nbsp;</td>
-    <td class="boton">
-      <input name="button" type="submit" class="button" value="Entrar">     </td>
-
-    </tr>
-</table>
-</form>
-</div>
-
-
-</div>
-  <!-- politico1 -->
-<div title="formulario" class="votaSobre">
-  <!-- confirmar -->
-  <!-- fin confirmar -->
-  <!-- login -->
-
-  <!-- fin login -->
+      <form action="<?php echo url_for('@sf_guard_signin') ?>" method="post">
+        <table id="login">
+          <tr>
+            <td class="leftSing"><h5><?php echo __('Tu Email') ?></h5></td>
+            <td>
+              <?php echo $signinform['username']->render() ?>
+              <?php echo $signinform['username']->renderError() ?>
+            </td>
+          </tr>
+          <tr>
+            <td class="leftSing"><h5><?php echo __('Contraseña') ?></h5></td>
+            <td>
+              <?php echo $signinform['password']->render() ?>
+              <?php echo $signinform['password']->renderError() ?>
+            </td>
+          </tr>
+          <tr>
+            <td class="leftSing"><h5><?php echo __('Recordar') ?></h5></td>
+            <td>
+              <?php echo $signinform['remember']->render() ?>
+              <?php echo $signinform['remember']->renderError() ?>
+            </td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td><h6><?php echo link_to(__('¿Olvidaste tu contraseña?'), 'sfGuardAuth/reminder') ?></h6></td>
+          </tr>
+          <tr>
+            <td class="leftSing">&nbsp;</td>
+            <td class="boton"><input name="button" type="submit" class="button" value="Entrar"></td>
+          </tr>
+        </table>
+      </form>
+    </div>
+  </div>
 </div>
 
 
