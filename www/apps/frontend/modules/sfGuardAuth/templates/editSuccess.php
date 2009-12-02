504856490003e6f61a09ea99233ef6eaae722e5d
Carlos Paramio <carlos@evolve.st>
2009-12-02 12:30:38 +0000

    reescritura de página de login
    git-svn-id: http://trac.voota.org/svn/voota/trunk@331 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- www/apps/frontend/modules/sfGuardAuth/templates/editSuccess.php
+++ www/apps/frontend/modules/sfGuardAuth/templates/editSuccess.php
@@ -5,141 +5,147 @@
 
 <!-- CONTENT LEFT-->
 <div id="contentLeftSing2">
-<div title="ficha">
-<h2><?php echo __('Hola %1%, estas son tus preferencias', array('%1%' => $sf_user->getProfile()->getNombre())) ?></h2>
-<?php echo showNotices( $sf_user ) ?>
+  <div title="ficha">
+    <h2><?php echo __('Hola %1%, estas son tus preferencias', array('%1%' => $sf_user->getProfile()->getNombre())) ?></h2>
+    <?php echo showNotices( $sf_user ) ?>
 
-<div class="limpiar"></div>
-<div class="formSing">
-<?php echo form_tag('@usuario_edit', 'multipart=true autocomplete=off') ?>
-<table id="editProfile">   
-  <tr>
-    <td class="anchoColumna tituloCampo tdperfil">
-      <?php if ($profileEditForm['nombre']->hasError()): ?><ul class="error_list"><li>&nbsp;</li></ul><?php endif ?>
-      <?php echo __('Tu nombre') ?>
-    </td>
-    <td class="wider tdperfil" >
-      <?php echo $profileEditForm['nombre']->renderError() ?>
-      <?php echo $profileEditForm['nombre']->render() ?>
-    </td>    
-  </tr>
-   
-  <tr>
-    <td class="anchoColumna tituloCampo tdperfil">
-      <?php if ($profileEditForm['apellidos']->hasError()): ?><ul class="error_list"><li>&nbsp;</li></ul><?php endif ?>
-      <?php echo __('Tus apellidos') ?>
-    </td>
-    <td class="wider tdperfil" >
-      <?php echo $profileEditForm['apellidos']->renderError() ?>
-      <?php echo $profileEditForm['apellidos']->render() ?>
-    </td>
-    <td class="tdperfil tips"><?php echo __('(opcional)') ?></td>
-  </tr>
-   
-  <tr>
-    <td class="anchoColumna tituloCampo tdperfil">
-      <?php if ($profileEditForm['presentacion']->hasError()): ?><ul class="error_list"><li>&nbsp;</li></ul><?php endif ?>
-      <?php echo __('Presentación') ?>
-    </td>
-    <td class="wider tdperfil" >
-      <?php echo $profileEditForm['presentacion']->renderError() ?>
-      <?php echo $profileEditForm['presentacion']->render(array('rows' => 15)) ?>
-    </td>
-    <td class="tdperfil tips"><?php echo __('(opcional)') ?></td>
-  </tr>
-
-  <tr>
-    <td class="anchoColumna tituloCampo tdperfil">
-      <?php if ($profileEditForm['fecha_nacimiento']->hasError()): ?><ul class="error_list"><li>&nbsp;</li></ul><?php endif ?>
-      <?php echo __('Fecha de nacimiento') ?>
-    </td>
-    <td class="wider tdperfil" >
-      <?php echo $profileEditForm['fecha_nacimiento']->renderError() ?>
-      <?php echo $profileEditForm['fecha_nacimiento']->render() ?>
-    </td>
-    <td class="tdperfil tips"><?php echo __('(opcional)') ?></td>
-  </tr>
-  
-  <tr>
-    <td class="anchoColumna tituloCampo tdperfil">
-      <?php if ($profileEditForm['vanity']->hasError()): ?><ul class="error_list"><li>&nbsp;</li></ul><?php endif ?>
-      <?php echo __('Tu página en Voota') ?> 
-    </td>
-    <td class="wider tdperfil" >
-      <?php echo $profileEditForm['vanity']->renderError() ?>
-      <span class="nombrePeque"><?php echo __('http://voota.es/') ?></span><?php echo $profileEditForm['vanity']->render(array('class' => 'inputSignVanity')) ?>
-    </td>    
-  </tr>
-  
-  <tr>
-    <td class="anchoColumna tituloCampo tdperfil">
-      <?php if ($profileEditForm['imagen']->hasError()): ?><ul class="error_list"><li>&nbsp;</li></ul><?php endif ?>
-      <?php echo __('Tu avatar') ?>
-    </td>
-    <td class="wider tdperfil" >
-      <?php echo $profileEditForm['imagen']->renderError() ?>
-      <?php echo $profileEditForm['imagen']->render() ?>
-    </td>
-    <td class="tdperfil tips"><?php echo __('(opcional)') ?></td>
-  </tr>
-   
-  <tr>
-    <td class="anchoColumna tituloCampo tdperfil">
-      <?php if ($profileEditForm['username']->hasError()): ?><ul class="error_list"><li>&nbsp;</li></ul><?php endif ?>
-      <?php echo __('Tu Email') ?>
-    </td>
-    <td class="wider tdperfil" >
-      <?php echo $profileEditForm['username']->renderError() ?>
-      <?php echo $profileEditForm['username']->render() ?>
-    </td>    
-  </tr>
-   
-  <tr>
-    <td class="anchoColumna tituloCampo tdperfil">
-      <?php if ($profileEditForm['passwordNew']->hasError()): ?><ul class="error_list"><li>&nbsp;</li></ul><?php endif ?>
-      <?php echo __('¿Nueva contraseña?') ?>
-    </td>
-    <td class="tdperfil" >
-      <?php echo $profileEditForm['passwordNew']->renderError() ?>
-      <?php echo $profileEditForm['passwordNew']->render() ?>
-    </td>
-    <td class="tdperfil tips"><?php echo __('(opcional)') ?></td>
-  </tr>
-   
-  <tr>
-    <td class="anchoColumna tituloCampo tdperfil">
-      <?php if ($profileEditForm['passwordBis']->hasError()): ?><ul class="error_list"><li>&nbsp;</li></ul><?php endif ?>
-      <?php echo __('Repítela nuevamente') ?>
-    </td>
-    <td class="tdperfil" >
-      <?php echo $profileEditForm['passwordBis']->renderError() ?>
-      <?php echo $profileEditForm['passwordBis']->render() ?>
-    </td>    
-  </tr>
-   
-  <tr>
-    <td class="anchoColumna tituloCampo tdperfil">
-      <?php if ($profileEditForm['passwordOld']->hasError()): ?><ul class="error_list"><li>&nbsp;</li></ul><?php endif ?>
-      <?php echo __('Contraseña actual') ?>
-    </td>
-    <td class="tdperfil" >
-      <?php echo $profileEditForm['passwordOld']->renderError() ?>
-      <?php echo $profileEditForm['passwordOld']->render() ?>
-    </td>    
-  </tr>
-  
-
-
-  <tr>
-    <td>&nbsp;</td>
-    <td class="tdperfil">
-    <?php echo submit_tag(__('Guardar cambios'), array('class'  => 'button')) ?>
-    
-    </td>
-    </tr>
-
-</table>
-</form>
-</div>
-</div>
+    <div class="limpiar"></div>
+    <div class="formSing">
+      <?php echo form_tag('@usuario_edit', 'multipart=true autocomplete=off') ?>
+        <table id="editProfile">   
+          <tr>
+            <td class="anchoColumna tituloCampo tdperfil">
+              <?php if ($profileEditForm['nombre']->hasError()): ?>
+                <ul class="error_list"><li>&nbsp;</li></ul>
+              <?php endif ?>
+              <?php echo __('Tu nombre') ?>
+            </td>
+            <td class="wider tdperfil" >
+              <?php echo $profileEditForm['nombre']->renderError() ?>
+              <?php echo $profileEditForm['nombre']->render() ?>
+            </td>
+          </tr>
+          <tr>
+            <td class="anchoColumna tituloCampo tdperfil">
+              <?php if ($profileEditForm['apellidos']->hasError()): ?>
+                <ul class="error_list"><li>&nbsp;</li></ul>
+              <?php endif ?>
+              <?php echo __('Tus apellidos') ?>
+            </td>
+            <td class="wider tdperfil" >
+              <?php echo $profileEditForm['apellidos']->renderError() ?>
+              <?php echo $profileEditForm['apellidos']->render() ?>
+            </td>
+            <td class="tdperfil tips"><?php echo __('(opcional)') ?></td>
+          </tr>
+          <tr>
+            <td class="anchoColumna tituloCampo tdperfil">
+              <?php if ($profileEditForm['presentacion']->hasError()): ?>
+                <ul class="error_list"><li>&nbsp;</li></ul>
+              <?php endif ?>
+              <?php echo __('Presentación') ?>
+            </td>
+            <td class="wider tdperfil" >
+              <?php echo $profileEditForm['presentacion']->renderError() ?>
+              <?php echo $profileEditForm['presentacion']->render(array('rows' => 15)) ?>
+            </td>
+            <td class="tdperfil tips"><?php echo __('(opcional)') ?></td>
+          </tr>
+          <tr>
+            <td class="anchoColumna tituloCampo tdperfil">
+              <?php if ($profileEditForm['fecha_nacimiento']->hasError()): ?>
+                <ul class="error_list"><li>&nbsp;</li></ul>
+              <?php endif ?>
+              <?php echo __('Fecha de nacimiento') ?>
+            </td>
+            <td class="wider tdperfil" >
+              <?php echo $profileEditForm['fecha_nacimiento']->renderError() ?>
+              <?php echo $profileEditForm['fecha_nacimiento']->render() ?>
+            </td>
+            <td class="tdperfil tips"><?php echo __('(opcional)') ?></td>
+          </tr>
+          <tr>
+            <td class="anchoColumna tituloCampo tdperfil">
+              <?php if ($profileEditForm['vanity']->hasError()): ?>
+                <ul class="error_list"><li>&nbsp;</li></ul>
+              <?php endif ?>
+              <?php echo __('Tu página en Voota') ?> 
+            </td>
+            <td class="wider tdperfil" >
+              <?php echo $profileEditForm['vanity']->renderError() ?>
+              <span class="nombrePeque"><?php echo __('http://voota.es/') ?></span><?php echo $profileEditForm['vanity']->render(array('class' => 'inputSignVanity')) ?>
+            </td>
+          </tr>
+          <tr>
+            <td class="anchoColumna tituloCampo tdperfil">
+              <?php if ($profileEditForm['imagen']->hasError()): ?>
+                <ul class="error_list"><li>&nbsp;</li></ul>
+              <?php endif ?>
+              <?php echo __('Tu avatar') ?>
+            </td>
+            <td class="wider tdperfil">
+              <?php echo $profileEditForm['imagen']->renderError() ?>
+              <?php echo $profileEditForm['imagen']->render() ?>
+            </td>
+            <td class="tdperfil tips"><?php echo __('(opcional)') ?></td>
+          </tr>
+          <tr>
+            <td class="anchoColumna tituloCampo tdperfil">
+              <?php if ($profileEditForm['username']->hasError()): ?>
+                <ul class="error_list"><li>&nbsp;</li></ul>
+              <?php endif ?>
+              <?php echo __('Tu Email') ?>
+            </td>
+            <td class="wider tdperfil">
+              <?php echo $profileEditForm['username']->renderError() ?>
+              <?php echo $profileEditForm['username']->render() ?>
+            </td>
+          </tr>
+          <tr>
+            <td class="anchoColumna tituloCampo tdperfil">
+              <?php if ($profileEditForm['passwordNew']->hasError()): ?>
+                <ul class="error_list"><li>&nbsp;</li></ul>
+              <?php endif ?>
+              <?php echo __('¿Nueva contraseña?') ?>
+            </td>
+            <td class="tdperfil" >
+              <?php echo $profileEditForm['passwordNew']->renderError() ?>
+              <?php echo $profileEditForm['passwordNew']->render() ?>
+            </td>
+            <td class="tdperfil tips"><?php echo __('(opcional)') ?></td>
+          </tr>
+          <tr>
+            <td class="anchoColumna tituloCampo tdperfil">
+              <?php if ($profileEditForm['passwordBis']->hasError()): ?>
+                <ul class="error_list"><li>&nbsp;</li></ul>
+              <?php endif ?>
+              <?php echo __('Repítela nuevamente') ?>
+            </td>
+            <td class="tdperfil">
+              <?php echo $profileEditForm['passwordBis']->renderError() ?>
+              <?php echo $profileEditForm['passwordBis']->render() ?>
+            </td>
+          </tr>
+          <tr>
+            <td class="anchoColumna tituloCampo tdperfil">
+              <?php if ($profileEditForm['passwordOld']->hasError()): ?>
+                <ul class="error_list"><li>&nbsp;</li></ul>
+              <?php endif ?>
+              <?php echo __('Contraseña actual') ?>
+            </td>
+            <td class="tdperfil">
+              <?php echo $profileEditForm['passwordOld']->renderError() ?>
+              <?php echo $profileEditForm['passwordOld']->render() ?>
+            </td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td class="tdperfil">
+              <?php echo submit_tag(__('Guardar cambios'), array('class'  => 'button')) ?>
+            </td>
+          </tr>
+        </table>
+      </form>
+    </div>
+  </div>
 </div>
--- www/apps/frontend/modules/sfGuardAuth/templates/signinSuccess.php
+++ www/apps/frontend/modules/sfGuardAuth/templates/signinSuccess.php
@@ -6,122 +6,92 @@
 
 <?php echo showNotices( $sf_user ) ?>
 
-<div id="contentLeftSing">
-  <div title="ficha">
-    <span class="tituloAzul"><?php echo __('¿Nuevo en Voota? Empieza aquí')?></span>
-    <div class="limpiar"></div>
+<div id="signup">
+  <h2><?php echo __('¿Nuevo en Voota? Empieza aquí')?></h2>
+  <?php echo form_tag('@sf_guard_signin') ?>
+    <?php echo input_hidden_tag('op', 'r') ?>
+    <table>
+      <tr>
+        <th><label for="registration_username"><?php echo __('Tu Email') ?></label></th>
+        <td>
+          <?php echo $registrationform['username']->render() ?>
+          <?php echo $registrationform['username']->renderError() ?>
+        </td>
+      </tr>
+      <tr>
+        <th><label for="registration_nombre"><?php echo __('Nombre') ?></label></th>
+        <td>
+          <?php echo $registrationform['nombre']->render() ?>
+          <?php echo $registrationform['nombre']->renderError() ?>
+        </td>
+      </tr>
+      <tr>
+        <th><label for="registration_apellidos"><?php echo __('Apellidos') ?></label></th>
+        <td>
+          <?php echo $registrationform['apellidos']->render() ?>
+          <?php echo $registrationform['apellidos']->renderError() ?>
+        </td>
+      </tr>
+      <tr>
+        <th><label for="registration_password"><?php echo __('Contraseña') ?></label></th>
+        <td>
+          <?php echo $registrationform['password']->render() ?>
+          <?php echo $registrationform['password']->renderError() ?>
+          <p class="hints">
+            <span id="show_pass_label"><a href="#" onclick="return showHidePass('registration_password')"><?php echo __('Ver la contraseña')?></a></span>
+  		      <span id="hide_pass_label" class="hidden"><a href="#" onclick="return showHidePass('registration_password')"><?php echo __('Ocultar la contraseña')?></a></span>
+          </p>
+        </td>
+      </tr>
+      <tr>
+        <th></th>
+        <td class="legal">
+          <label for="registration_accept">He leído y acepto el <?php echo link_to(__('aviso legal'), __('http://blog.voota.es/es/aviso-legal'), array()) ?><?php echo $registrationform['accept']->render() ?></label>
+          <?php echo $registrationform['accept']->renderError() ?>
+        </td>
+      </tr>
+      <tr>
+        <th></th>
+        <td class="submit">
+          <?php echo submit_tag(__('Registrate en Voota')) ?>
+        </td>
+      </tr>
+    </table>
+  </form>
+</div><!-- FIN SIGNUP -->
 
-    <div class="formSing">
-      <?php echo form_tag('@sf_guard_signin') ?>
-	      <?php echo input_hidden_tag('op', 'r') ?>
-
-        <table id="signin">
-          <tr>
-            <td class="leftSing"><h5><?php echo __('Tu email') ?></h5></td>
-            <td class="campo">
-              <?php echo $registrationform['username']->render() ?>
-              <?php echo $registrationform['username']->renderError() ?>
-            </td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td class="leftSing"><h5>
-              <?php echo __('Nombre') ?>
-            </h5></td>
-            <td class="campo">
-              <?php echo $registrationform['nombre']->render() ?>
-              <?php echo $registrationform['nombre']->renderError() ?>
-            </td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td class="leftSing"><h5><?php echo __('Apellidos') ?></h5></td>
-            <td class="campo">
-              <?php echo $registrationform['apellidos']->render() ?>
-              <?php echo $registrationform['apellidos']->renderError() ?>
-            </td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td class="leftSing"><h5><?php echo __('Contraseña') ?></h5></td>
-            <td class="campo">
-              <?php echo $registrationform['password']->render() ?>
-              <?php echo $registrationform['password']->renderError() ?>
-            </td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td>&nbsp;</td>
-            <td>
-              <h6>
-                <span id="show_pass_label"><a href="#" onclick="return showHidePass('registration_password')"><?php echo __('Ver la contraseña')?></a></span>
-    		        <span id="hide_pass_label" class="hidden"><a href="#" onclick="return showHidePass('registration_password')"><?php echo __('Ocultar la contraseña')?></a></span>
-    	        </h6>
-    	      </td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td class="leftSing"></td>
-            <td class="legal">
-              He leído y acepto el <?php echo link_to(__('aviso legal'), __('http://blog.voota.es/es/aviso-legal'), array()) ?><?php echo $registrationform['accept']->render() ?>
-              <?php echo $registrationform['accept']->renderError() ?>
-            </td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td class="leftSing">&nbsp;</td>
-            <td class="boton">
-              <?php echo submit_tag(__('Registrate en Voota'), array('class'   => 'button',)) ?>
-            </td>
-            <td>&nbsp;</td>
-          </tr>
-        </table>
-      </form>
-    </div>
-    <div class="limpiar"></div>
-  </div>
-</div><!-- FIN CONTENT LEFT -->
-
-<!-- CONTENT RIGHT -->
-<div id="contentRightSing">
-  <div title="ficha">
-    <span class="tituloAzul"><?php echo __('¿Ya estás registrado? Adelante :)')?></span>
-    <div class="formSing">
-      <form action="<?php echo url_for('@sf_guard_signin') ?>" method="post">
-        <table id="login">
-          <tr>
-            <td class="leftSing"><h5><?php echo __('Tu Email') ?></h5></td>
-            <td>
-              <?php echo $signinform['username']->render() ?>
-              <?php echo $signinform['username']->renderError() ?>
-            </td>
-          </tr>
-          <tr>
-            <td class="leftSing"><h5><?php echo __('Contraseña') ?></h5></td>
-            <td>
-              <?php echo $signinform['password']->render() ?>
-              <?php echo $signinform['password']->renderError() ?>
-            </td>
-          </tr>
-          <tr>
-            <td class="leftSing"><h5><?php echo __('Recordar') ?></h5></td>
-            <td>
-              <?php echo $signinform['remember']->render() ?>
-              <?php echo $signinform['remember']->renderError() ?>
-            </td>
-          </tr>
-          <tr>
-            <td>&nbsp;</td>
-            <td><h6><?php echo link_to(__('¿Olvidaste tu contraseña?'), 'sfGuardAuth/reminder') ?></h6></td>
-          </tr>
-          <tr>
-            <td class="leftSing">&nbsp;</td>
-            <td class="boton"><input name="button" type="submit" class="button" value="Entrar"></td>
-          </tr>
-        </table>
-      </form>
-    </div>
-  </div>
+<div id="login">
+  <h2><?php echo __('¿Ya estás registrado? Adelante :)')?></h2>
+  <form action="<?php echo url_for('@sf_guard_signin') ?>" method="post">
+    <table>
+      <tr>
+        <th><label for="signin_username"><?php echo __('Tu Email') ?></label></th>
+        <td>
+          <?php echo $signinform['username']->render() ?>
+          <?php echo $signinform['username']->renderError() ?>
+        </td>
+      </tr>
+      <tr>
+        <th><label for="signin_password"><?php echo __('Contraseña') ?></label></th>
+        <td>
+          <?php echo $signinform['password']->render() ?>
+          <?php echo $signinform['password']->renderError() ?>
+          <p class="hints"><?php echo link_to(__('¿Olvidaste tu contraseña?'), 'sfGuardAuth/reminder') ?></p>
+        </td>
+      </tr>
+      <tr>
+        <th><label for="signin_remember"><?php echo __('Recordar') ?></label></th>
+        <td>
+          <?php echo $signinform['remember']->render() ?>
+          <?php echo $signinform['remember']->renderError() ?>
+        </td>
+      </tr>
+      <tr>
+        <th></th>
+        <td class="submit"><input name="button" type="submit" class="button" value="Entrar"></td>
+      </tr>
+    </table>
+  </form>
 </div>
 
 
