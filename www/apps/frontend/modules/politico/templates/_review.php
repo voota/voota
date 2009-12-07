ff4cc4b60ab1a1ae7d99f43ce14f7821fb5a0907
Carlos Paramio <carlos@evolve.st>
2009-12-07 10:37:04 +0000

    nueva maquetación de opiniones
    git-svn-id: http://trac.voota.org/svn/voota/trunk@344 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- www/apps/frontend/modules/politico/templates/_review.php
+++ www/apps/frontend/modules/politico/templates/_review.php
@@ -2,21 +2,23 @@
 <?php use_helper('I18N') ?>
 
 <li class="review">
-	<div class="review_img">
-<?php if( $review->getsfGuardUser()->getProfile()->getImagen() ): ?>
-<?php echo image_tag('http://'.S3Voota::getBucketPub().'.s3.amazonaws.com/usuarios/cc_s_'.( $review->getsfGuardUser()->getProfile()->getImagen()), 'alt="'.  $review->getsfGuardUser()->getProfile()->getNombre().' ' .  $review->getsfGuardUser()->getProfile()->getApellidos() .'"') ?>
-<?php else: ?>
-<?php echo image_tag('http://'.S3Voota::getBucketPub().'.s3.amazonaws.com/usuarios/v.png', 'alt="'.  $review->getsfGuardUser()->getProfile()->getNombre().' ' .  $review->getsfGuardUser()->getProfile()->getApellidos() .'"') ?>
-<?php endif ?>
+	<div class="review-avatar">
+    <?php if( $review->getsfGuardUser()->getProfile()->getImagen() ): ?>
+      <?php echo image_tag('http://'.S3Voota::getBucketPub().'.s3.amazonaws.com/usuarios/cc_s_'.( $review->getsfGuardUser()->getProfile()->getImagen()), array('alt' => $review->getsfGuardUser()->getProfile()->getNombre().' ' .  $review->getsfGuardUser()->getProfile()->getApellidos(), 'width' => 36, 'height' => 36)) ?>
+    <?php else: ?>
+      <?php echo image_tag('http://'.S3Voota::getBucketPub().'.s3.amazonaws.com/usuarios/v.png', array('alt' => $review->getsfGuardUser()->getProfile()->getNombre().' ' .  $review->getsfGuardUser()->getProfile()->getApellidos(), 'width' => 36, 'height' => 36)) ?>
+    <?php endif ?>
 	</div>
-	<h6><?php echo $review->getsfGuardUser()->getProfile()->getNombre(); ?> <?php echo $review->getsfGuardUser()->getProfile()->getApellidos(); ?>
-		 <?php /* ?>· <span class="lugar">Madrid<?php */ ?>
-		 <?php if( $review->getsfGuardUser()->getProfile()->getFechaNacimiento() ): ?>
-		 	· <?php echo __('%1% años', array('%1%' => review_date_diff( $review->getsfGuardUser()->getProfile()->getFechaNacimiento() )))?>
-		 <?php endif ?>
-		 <br>
-		 <span class="review_date"><?php echo ago(strtotime( $review->getModifiedAt()?$review->getModifiedAt():$review->getCreatedAt() ))?></span>
-		 <br>
-		<?php echo review_text( $review ) ?>
-	</h6>
+  <h4 class="review-name">
+    <?php echo $review->getsfGuardUser()->getProfile()->getNombre(); ?> <?php echo $review->getsfGuardUser()->getProfile()->getApellidos(); ?>
+    <?php if( $review->getsfGuardUser()->getProfile()->getFechaNacimiento() ): ?>
+      <span class="review-years">· <?php echo __('%1% años', array('%1%' => review_date_diff( $review->getsfGuardUser()->getProfile()->getFechaNacimiento() )))?></span>
+    <?php endif ?>
+  </h4>
+	<p class="review-date">
+	  <?php echo ago(strtotime( $review->getModifiedAt()?$review->getModifiedAt():$review->getCreatedAt() ))?>
+	</p>
+  <p class="review-body">
+    <?php echo review_text( $review ) ?>
+  </p>
 </li>
