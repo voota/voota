f22cd7dfddf7ec5785eae404f04fd695f80b411f
Carlos Paramio <carlos@evolve.st>
2009-12-09 12:07:24 +0000

    refactorización y limpieza de formulario de opinión
    git-svn-id: http://trac.voota.org/svn/voota/trunk@360 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- www/plugins/sfReviewPlugin/modules/sfReviewFront/templates/formSuccess.php
+++ www/plugins/sfReviewPlugin/modules/sfReviewFront/templates/formSuccess.php
@@ -3,49 +3,49 @@
 <?php use_helper('jQuery') ?>
 	
 <script type="text/javascript">
-<!--
-<?php if($reviewId == ''): ?>
-	$("#<?php echo ($reviewBox?$reviewBox:'sf_review').'_button' ?>").click(function () {
-		if (!<?php echo ($reviewBox?$reviewBox:'sf_review').'_form' ?>_edited){
-			$('textarea#sf_review_text').val('');
-		}
-		return true;
-	});
-<?php endif ?>
-<?php if($reviewId == ''): ?>
-var <?php echo ($reviewBox?$reviewBox:'sf_review').'_form' ?>_edited = false;
-$('textarea#sf_review_text').focus(function() {
-	MAX_LENGTH = <?php echo $maxLength?>;
+  <!--
+  <?php if($reviewId == ''): ?>
+  	$("#<?php echo ($reviewBox?$reviewBox:'sf_review').'_button' ?>").click(function () {
+  		if (!<?php echo ($reviewBox?$reviewBox:'sf_review').'_form' ?>_edited){
+  			$('#sf-review-text').val('');
+  		}
+  		return true;
+  	});
+  <?php endif ?>
+  <?php if($reviewId == ''): ?>
+    var <?php echo ($reviewBox?$reviewBox:'sf_review').'_form' ?>_edited = false;
+    $('#sf-review-text').focus(function() {
+    	MAX_LENGTH = <?php echo $maxLength?>;
 	
-	if (!<?php echo ($reviewBox?$reviewBox:'sf_review').'_form' ?>_edited){
-		$(this).val('');
-		$(this).removeClass('sfr_grey');
-		$('span#sf_review_counter').html(MAX_LENGTH);
-		<?php echo ($reviewBox?$reviewBox:'sf_review').'_form' ?>_edited = true;
-	}
-});
-<?php endif ?>
-//controls character input/counter
-$('textarea#sf_review_text').keyup(function() {
-	MAX_LENGTH = <?php echo $maxLength?>;
+    	if (!<?php echo ($reviewBox?$reviewBox:'sf_review').'_form' ?>_edited){
+    		$(this).val('');
+    		$(this).removeClass('sfr_grey');
+    		$('#sf-review-counter').html(MAX_LENGTH);
+    		<?php echo ($reviewBox?$reviewBox:'sf_review').'_form' ?>_edited = true;
+    	}
+    });
+  <?php endif ?>
+  //controls character input/counter
+  $('#sf-review-text').keyup(function() {
+  	MAX_LENGTH = <?php echo $maxLength?>;
 	
-	var charLength = $(this).val().length;
-	if((MAX_LENGTH - charLength) < -50) {
-		$(this).val( $(this).val().substr(0, MAX_LENGTH+50) );
-		charLength = $(this).val().length;
-	}
-	else if((MAX_LENGTH - charLength) < 0) {
-		$('span#sf_review_counter').attr('style', 'color:red;');
-	}
-	else if((MAX_LENGTH - charLength) < 40) {
-		$('span#sf_review_counter').attr('style', 'color:orange;');
-	}
-	else {
-		$('span#sf_review_counter').attr('style', '');
-	}
-	$('span#sf_review_counter').html(MAX_LENGTH - charLength);
-});
-//-->
+  	var charLength = $(this).val().length;
+  	if((MAX_LENGTH - charLength) < -50) {
+  		$(this).val( $(this).val().substr(0, MAX_LENGTH+50) );
+  		charLength = $(this).val().length;
+  	}
+  	else if((MAX_LENGTH - charLength) < 0) {
+  		$('#sf-review-counter').attr('style', 'color:red;');
+  	}
+  	else if((MAX_LENGTH - charLength) < 40) {
+  		$('#sf-review-counter').attr('style', 'color:orange;');
+  	}
+  	else {
+  		$('#sf-review-counter').attr('style', '');
+  	}
+  	$('#sf-review-counter').html(MAX_LENGTH - charLength);
+  });
+  //-->
 </script>
 
 <?php echo jq_form_remote_tag(array(
@@ -59,80 +59,77 @@ $('textarea#sf_review_text').keyup(function() {
 	<?php echo input_hidden_tag('i', $reviewId) ?>
 
 	<?php if ($reviewId != ''): ?>
-		<div class="sfr_left"><span class="sfr_title"><?php echo __('Tu opinión') ?></span></div>
-		<div>
-			<span class="sfr">&nbsp;
-				<?php echo jq_link_to_remote(__('Dejarlo como estaba'), array(
+		<h5>
+		  <?php echo __('Tu opinión') ?>
+		  <span class="sf-review-cancel">
+		    <?php echo jq_link_to_remote(__('Dejarlo como estaba'), array(
 				    'update'   => $reviewBox?$reviewBox:'sf_review',
 				    'url'    => "@sf_review_init?i=$reviewId&e=$reviewEntityId&t=$reviewType".($reviewBox==''?'':"&b=$reviewBox"),
 				)) ?>
-			</span>
-		</div>
+		  </span>
+		</h5>
 	<?php endif?>
-	<div class="sfr_hands">
-		<div class="sfr_margin">
+	<div class="sf-review-hands">
+    <div class="sf-review-positive">
 			<?php echo radiobutton_tag('v', '1', ($reviewValue==1)?true:false) ?>
-		
-		<?php echo image_tag('icoUp.gif', 'alt="yeah"') ?>
-		  <br>
-		  <span class="sfr"><?php echo __('A favor, yeah')?></span> </div>
-		
-		<div>
+		  <label for="v_1">
+		    <?php echo image_tag('icoUp.gif', 'alt="yeah"') ?>
+  		  <br />
+		    <?php echo __('A favor, yeah')?>
+		  </label>
+		</div>
+
+		<div class="sf-review-negative">
 			<?php echo radiobutton_tag('v', '-1', ($reviewValue==-1)?true:false) ?>
-		<?php echo image_tag('icoDown.gif', 'alt="buu"') ?>
-		  <br>
-		
-		  <span class="sfr"><?php echo __('En contra, buu')?></span>
+			<label for="v_-1">
+		    <?php echo image_tag('icoDown.gif', 'alt="buu"') ?>
+		    <br />
+		    <?php echo __('En contra, buu')?>
+		  </label>
 		</div>
 	</div>
 
-	<div title="formulario">
-		<div>
-		  <?php $aReviewText = $reviewText || $reviewId != ''?$reviewText:__('Algo que comentar? Es el mejor momento :-)') ?>
-		  <span id="sf_review_counter"><?php echo $maxLength - strlen( $aReviewText )?></span>
-		  <textarea id="sf_review_text" name="review_text" class="sfr<?php if($reviewId == ''): ?> sfr_grey<?php endif ?>"><?php echo $aReviewText ?></textarea>
-		  <?php if ($reviewId != ''): ?>
-			  
-			  <?php if (!isset($cf)): ?>
-			    <span class="sfr">
+  <?php $aReviewText = $reviewText || $reviewId != ''?$reviewText:__('¿Algo que comentar? Es el mejor momento :-)') ?>
+  <p id="sf-review-counter"><?php echo $maxLength - strlen( $aReviewText )?></p>
+  <p id="sf-review-body">
+    <textarea id="sf-review-text" name="review_text" class="sfr<?php if($reviewId == ''): ?> sfr_grey<?php endif ?>"><?php echo $aReviewText ?></textarea>
+  </p>
+
+  <?php if ($reviewId != ''): ?>
+	  <?php if (!isset($cf)): ?>
+	    <p class="sf-review-remove">
 				<?php echo jq_link_to_remote(__('Eliminar opinión'), array(
-				    'update'   => $reviewBox?$reviewBox:'sf_review',
-				    'url'    => "@sf_review_delete?i=$reviewId&e=$reviewEntityId&t=$reviewType".($reviewBox==''?'':"&b=$reviewBox"),
+			    'update'   => $reviewBox?$reviewBox:'sf_review',
+			    'url'    => "@sf_review_delete?i=$reviewId&e=$reviewEntityId&t=$reviewType".($reviewBox==''?'':"&b=$reviewBox"),
 				)) ?>
-				</span>
-			  <?php else: ?>
-			  	<div class="sfr_margin">
-				  	<span class="sfr"><?php echo __('¿Seguro?') ?></span>
-				</div> 
-			  	<div class="sfr_margin">
-				<span class="sfr"><?php echo jq_link_to_remote(__('Sí'), array(
-				    'update'   => $reviewBox?$reviewBox:'sf_review',
-				    'url'    => "@sf_review_delete?i=$reviewId&e=$reviewEntityId&t=$reviewType&cf=1".($reviewBox==''?'':"&b=$reviewBox"),
-				)) ?></span>
-				</div>
-			  	<div class="sfr_left">
-				<span class="sfr"><?php echo jq_link_to_remote(__('No'), array(
-				    'update'   => $reviewBox?$reviewBox:'sf_review',
-				    'url'    => "@sf_review_form?i=$reviewId&e=$reviewEntityId&t=$reviewType".($reviewBox==''?'':"&b=$reviewBox"),
-				)) ?></span>
-				</div>
-			  <?php endif ?>
-			
-		  <?php endif ?>
-		  <?php 
-		  /*
-		  <span class="sfr">Foto/vídeo: <a href="#">desde el equipo</a> · <a href="#">desde la web</a></span>
-		    <span class="sfr"><label>
-		    <input name="fileField" type="file" id="fileField">
-		    </label></span>
-		   */?>
-		   <span class="sfr">&nbsp;</span>
-		    <label>
-		    
-	<?php echo submit_tag(__('Enviar'), array('class' => 'sfr_button', 'id' => ($reviewBox?$reviewBox:'sf_review').'_button' )) ?>
-		    </label>
-		
-		</div>
-	</div>
-	
+			</p>
+	  <?php else: ?>
+	  	<p class="sf-review-remove">
+	  	  <?php echo __('¿Seguro?') ?>
+	  	  <span>
+  	  	  <?php echo jq_link_to_remote(__('Sí'), array(
+  			    'update'   => $reviewBox?$reviewBox:'sf_review',
+  			    'url'    => "@sf_review_delete?i=$reviewId&e=$reviewEntityId&t=$reviewType&cf=1".($reviewBox==''?'':"&b=$reviewBox"),
+  			  )) ?>
+			  </span>
+			  <span>
+			    <?php echo jq_link_to_remote(__('No'), array(
+			      'update'   => $reviewBox?$reviewBox:'sf_review',
+			      'url'    => "@sf_review_form?i=$reviewId&e=$reviewEntityId&t=$reviewType".($reviewBox==''?'':"&b=$reviewBox"),
+			)) ?>
+			  </span>
+			</p>
+		<?php endif ?>
+  <?php endif ?>
+
+  <?php 
+	  /*
+	  <p>Foto/vídeo: <a href="#">desde el equipo</a> · <a href="#">desde la web</a>
+	    <input name="fileField" type="file" id="fileField">
+	  </p>
+  */?>
+
+  <p>
+    <?php echo submit_tag(__('Enviar'), array('class' => 'sfr_button', 'id' => ($reviewBox?$reviewBox:'sf_review').'_button' )) ?>
+  </p>
 </form>
