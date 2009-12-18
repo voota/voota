f9d804d385e3cd1da41ba8071612dd9479e410ea
Carlos Paramio <carlos@evolve.st>
2009-12-18 16:07:07 +0000

    maquetación de página de despedida, separación de enlaces de confirmación
    git-svn-id: http://trac.voota.org/svn/voota/trunk@434 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- www/apps/frontend/modules/sfGuardAuth/templates/removeSuccess.php
+++ www/apps/frontend/modules/sfGuardAuth/templates/removeSuccess.php
@@ -1,13 +1,6 @@
 <?php use_helper('I18N') ?>
-<!-- CONTENT LEFT-->
-<div id="contentLeftSing2">
-  <div title="ficha">
-    <h2><?php echo __('Borrado completado. Te echaremos de menos.')?></h2>
-    <div class="limpiar"></div>
-  </div>
-  <div class="formSing">
-    <h5><?php echo __('¿Que hacemos ahora?')?><span class="tituloAzul"></span> <?php echo __('Tú dirás.')?></h5>
-    <h5><?php echo __('¿Nos vamos a la')?> <?php echo link_to("home de Voota", "@homepage") ?>?</h5>
-    <div class="limpiar"></div>
-  </div>
-</div>
+
+<h2><?php echo __('Borrado completado. Te echaremos de menos.')?></h2>
+
+<p class="next-step-msg"><?php echo __('¿Qué hacemos ahora?')?><span class="tituloAzul"></span> <?php echo __('Tú dirás.')?></p>
+<p class="next-step-msg"><?php echo __('¿Nos vamos a la')?> <?php echo link_to("home de Voota", "@homepage") ?>?</p>
