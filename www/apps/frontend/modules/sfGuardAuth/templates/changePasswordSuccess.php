fe5e8c94813c473448e48451f2d5e47b71ed3046
Carlos Paramio <carlos@evolve.st>
2009-12-21 12:22:07 +0000

    página de editar contraseña
    git-svn-id: http://trac.voota.org/svn/voota/trunk@442 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- www/apps/frontend/modules/sfGuardAuth/templates/changePasswordSuccess.php
+++ www/apps/frontend/modules/sfGuardAuth/templates/changePasswordSuccess.php
@@ -2,55 +2,32 @@
 <?php use_helper('I18N') ?>
 <?php use_helper('jQuery') ?>
 <?php use_helper('Validation') ?>
-
 <?php use_helper('VoNotice') ?>
+
+<h2><?php echo __('Define tu nueva contraseña')?></h2>
 
 <?php echo showNotices( $sf_user ) ?>
 
-<!-- CONTENT LEFT-->
-<div id="contentLeftSing2">
-  <div title="ficha">
-    <h2><?php echo __('Define tu nueva contraseña')?></h2>
-
-    <div class="limpiar"></div>
-    <div class="formSing">
-      <?php echo form_tag('@usuario_change_password2') ?>
-        <?php echo input_hidden_tag('codigo', $codigo) ?>
-
-        <table>
-          <tr>
-            <td class="anchoColumna tituloCampo tdperfil">
-              <?php echo __('Contraseña') ?>
-            </td>
-            <td class="leftSing tdperfil" >
-              <?php echo $form['passwordNew']->renderError() ?>
-              <?php echo $form['passwordNew']->render() ?>
-            </td>    
-          </tr>
-          <tr>
-            <td class="anchoColumna tituloCampo tdperfil">
-              <?php echo __('Otra vez') ?>
-            </td>
-            <td class="leftSing tdperfil" >
-              <?php echo $form['password_again']->renderError() ?>
-              <?php echo $form['password_again']->render() ?>
-            </td>    
-          </tr>
-          <tr>
-            <td>&nbsp;</td>
-            <td class="alingBoton">
-              <?php echo submit_tag('Enviar', array('class'   => 'button',)) ?>
-            </td>
-          </tr>
-        </table>
-      </form>
-    </div>
-
-    <div class="limpiar"></div>
-    <!--usuario -->
-    <div class="limpiar"></div>
-    <!--contraseña -->
-  </div>
-</div>
-
-
+<?php echo form_tag('@usuario_change_password2') ?>
+  <?php echo input_hidden_tag('codigo', $codigo) ?>
+  <table>
+    <tr>
+      <th><label for="profile_passwordNew"><?php echo __('Contraseña') ?></label></th>
+      <td>
+        <?php echo $form['passwordNew']->render() ?>
+        <?php echo $form['passwordNew']->renderError() ?>
+      </td>
+    </tr>
+    <tr>
+      <th><label for="profile_password_again"><?php echo __('Otra vez') ?></label></th>
+      <td>
+        <?php echo $form['password_again']->render() ?>
+        <?php echo $form['password_again']->renderError() ?>
+      </td>
+    </tr>
+    <tr>
+      <th></th>
+      <td class="submit"><?php echo submit_tag('Enviar', array('class'   => 'button',)) ?></td>
+    </tr>
+  </table>
+</form>
