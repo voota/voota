2a0425b15fea1736ceb4b209ba6db31a19b5c5d8
Sergio Viteri <sergio@infovit.net>
2009-12-30 17:41:22 +0000

    git-svn-id: http://trac.voota.org/svn/voota/trunk@486 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- www/apps/frontend/modules/home/templates/_institucion_top.php
+++ www/apps/frontend/modules/home/templates/_institucion_top.php
@@ -1,7 +1,7 @@
 <li>
 	<div class="avatar">
 	  <?php if($institucion->getImagen()!=''): ?>
-    	<?php echo image_tag('http://'.S3Voota::getBucketPub().'.s3.amazonaws.com/instituciones/cc_s_'.($institucion->getImagen()!=''?$institucion->getImagen():'p_unknown.png'), 'alt="Foto de '. $institucion .'"') ?>
+    	<?php echo image_tag('http://'.S3Voota::getBucketPub().'.s3.amazonaws.com/instituciones/cc_s_'.($institucion->getImagen()!=''?$institucion->getImagen():'p_unknown.png'), 'alt="'. __('Foto de %1%', array('%1%' => $institucion)) .'"') ?>
 	  <?php endif ?>
     </div>
 	<h4 class="name"><?php echo link_to($institucion->getNombre(), 'politico/ranking?partido=all&institucion='.$institucion->getVanity())?></h4>
