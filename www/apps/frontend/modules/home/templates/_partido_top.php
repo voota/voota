4d23e510fe96cece8807358ec450f7ca310a8b8d
Sergio Viteri <sergio@infovit.net>
2010-01-15 13:07:13 +0000

    git-svn-id: http://trac.voota.org/svn/voota/trunk@571 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- www/apps/frontend/modules/home/templates/_partido_top.php
+++ www/apps/frontend/modules/home/templates/_partido_top.php
@@ -1,8 +1,6 @@
 <li>
 	<div class="avatar">
-	  <?php if($partido->getImagen()!=''): ?>
     	<?php echo image_tag('http://'.S3Voota::getBucketPub().'.s3.amazonaws.com/partidos/cc_s_'.($partido->getImagen()!=''?$partido->getImagen():'p_unknown.png'), 'alt="'. __('Foto de %1%', array('%1%' => $partido)) .'"') ?>
-	  <?php endif ?>
     </div>
 	<h4 class="name"><?php echo link_to($partido->getNombre()." (".$partido->getAbreviatura().")", 'partido/show?id='.$partido->getAbreviatura())?></h4>
 </li>
