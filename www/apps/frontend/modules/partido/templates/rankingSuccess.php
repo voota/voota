04849c0c5852dadc2d06f592b7d4e26fba864d1e
Carlos Paramio <carlos@evolve.st>
2010-01-19 11:59:15 +0000

    nombres de partidos completos en ranking
    git-svn-id: http://trac.voota.org/svn/voota/trunk@648 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- www/apps/frontend/modules/partido/templates/rankingSuccess.php
+++ www/apps/frontend/modules/partido/templates/rankingSuccess.php
@@ -72,7 +72,7 @@ $(document).ready(function(){
           <?php echo image_tag('http://'.S3Voota::getBucketPub().'.s3.amazonaws.com/partidos/cc_s_'.($partido->getImagen()!=''?$partido->getImagen():'p_unknown.png'), 'alt="'. __('Foto de %1%', array('%1%' => $partido)) .'"') ?>
   	    </td>
         <td class="name">
-          <?php echo link_to(	$partido->getAbreviatura(), 'partido/show?id='.$partido->getAbreviatura() ); ?>
+          <?php echo link_to(	$partido->getNombre() . ' (' . $partido->getAbreviatura() . ')', 'partido/show?id='.$partido->getAbreviatura() ); ?>
         </td>
         <td class="positive-votes"><?php echo $partido->getSumu()?></td>
         <td class="negative-votes"><?php echo $partido->getSumd()?></td>
