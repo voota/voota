32293844ab9d7b8875e279433f06dfa944fbb2a3
Carlos Paramio <carlos@evolve.st>
2010-01-20 09:54:30 +0000

    formulario de contacto sin maquetar
    git-svn-id: http://trac.voota.org/svn/voota/trunk@653 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- www/apps/frontend/modules/perfil/templates/contactSuccess.php
+++ www/apps/frontend/modules/perfil/templates/contactSuccess.php
@@ -4,14 +4,16 @@
 <?php use_helper('VoFormat') ?>
 
 
-
-  <div class="profile">
-    <h2>
-      Contactar  a <?php echo $user ?>
-      <?php if ($sf_user->isAuthenticated() && $sf_user->getGuardUser()->getId() == $user->getId()): ?>
-        <?php echo link_to(__('Hacer cambios en tu perfil'), "@usuario_edit"); ?>
-      <?php endif ?>
-    </h2>
-  
-
+<div class="profile">
+  <h2>
+    Mandar un mensaje a <a href="#"><?php echo $user ?></a>
+    <?php if($user->getProfile() && $user->getProfile()->getImagen() && $user->getProfile()->getImagen() != '' ): ?>
+      <?php echo image_tag('http://'.S3Voota::getBucketPub().'.s3.amazonaws.com/usuarios/cc_s_'.($user->getProfile()->getImagen()), 'alt="Foto '. $user->getProfile()->getNombre().' ' . $user->getProfile()->getApellidos() .'"') ?>
+    <?php endif ?>
+  </h2>
+  <form action="#" method="post" accept-charset="utf-8">
+    <p><label for="body">¿Qué le decimos?</label></p>
+    <p><textarea id="body"></textarea></p>
+    <p class="submit"><input type="submit" value="Enviar" /></p>
+  </form>
 </div>
