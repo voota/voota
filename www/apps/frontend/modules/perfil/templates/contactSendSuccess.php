6d15f302fc183ed3b8aef3000cef5e853b52a221
Carlos Paramio <carlos@evolve.st>
2010-01-20 10:22:46 +0000

    estilos mensaje confirmación contacto
    git-svn-id: http://trac.voota.org/svn/voota/trunk@656 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- www/apps/frontend/modules/perfil/templates/contactSendSuccess.php
+++ www/apps/frontend/modules/perfil/templates/contactSendSuccess.php
@@ -1,14 +1,16 @@
 <?php use_helper('I18N') ?>
+<?php use_helper('jQuery') ?>
 
-<h2><?php echo __('Contacta con Voota') ?></h2>
-<p class="success-msg"><?php echo __('Gracias por tu mensaje') ?></p>
+<h2>
+  <img src="/images/icoOk.gif" alt="Ok" width="46" height="39" />
+  Ok, mensaje enviado a <a href="#"><?php echo $user ?></a>
+  <?php if($user->getProfile() && $user->getProfile()->getImagen() && $user->getProfile()->getImagen() != '' ): ?>
+    <?php echo image_tag('http://'.S3Voota::getBucketPub().'.s3.amazonaws.com/usuarios/cc_s_'.($user->getProfile()->getImagen()), 'alt="Foto '. $user->getProfile()->getNombre().' ' . $user->getProfile()->getApellidos() .'"') ?>
+  <?php endif ?>
+</h2>
 
 <div id="content">
   <p class="next-step-msg"><?php echo __('¿Que hacemos ahora?') ?> <?php echo __('Tú dirás.') ?></p>
   <p class="next-step-msg"><?php echo __('¿Nos vamos a la')?> <?php echo link_to("home de Voota", "@homepage") ?>?</p>
   <p class="next-step-msg"><?php echo __('¿Una vuelta por el')?> <?php echo link_to(__("ranking de políticos"), "politico/ranking") ?> <?php echo __('quizás?') ?></p>
-</div>
-
-<div id="sidebar">
-  <?php //include_partial('socialBox') ?>
 </div>
