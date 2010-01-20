278399f09677313015273536d9188198887b5bfd
Sergio Viteri <sergio@infovit.net>
2010-01-20 12:34:32 +0000

    git-svn-id: http://trac.voota.org/svn/voota/trunk@663 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- www/apps/frontend/templates/layout.php
+++ www/apps/frontend/templates/layout.php
@@ -1,5 +1,6 @@
 <?php use_helper('I18N') ?>
 <?php use_helper('jQuery') ?>
+
 <?php echo "<?xml version='1.0' encoding='utf-8' ?>" ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns='http://www.w3.org/1999/xhtml'>
