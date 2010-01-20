a5057b7ff7aeb74f166162c541e41610b3a7d78c
Carlos Paramio <carlos@evolve.st>
2010-01-20 10:12:58 +0000

    estilos formulario contacto
    git-svn-id: http://trac.voota.org/svn/voota/trunk@655 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- www/apps/frontend/modules/perfil/templates/contactSuccess.php
+++ www/apps/frontend/modules/perfil/templates/contactSuccess.php
@@ -3,17 +3,27 @@
 <?php use_helper('jQuery') ?>
 <?php use_helper('VoFormat') ?>
 
+<script type="text/javascript" charset="utf-8">
+  <!--//
+  $(document).ready(function() {
+    //controls character input/counter
+    $('#mensaje_cuerpo').keyup(function() {
+  	  setCounter('#cuerpo_counter', this, 280);
+    });
+    setCounter('#cuerpo_counter', '#mensaje_cuerpo', 280);
+  });
+  //-->
+</script>
 
-<div class="profile">
-  <h2>
-    Mandar un mensaje a <a href="#"><?php echo $user ?></a>
-    <?php if($user->getProfile() && $user->getProfile()->getImagen() && $user->getProfile()->getImagen() != '' ): ?>
-      <?php echo image_tag('http://'.S3Voota::getBucketPub().'.s3.amazonaws.com/usuarios/cc_s_'.($user->getProfile()->getImagen()), 'alt="Foto '. $user->getProfile()->getNombre().' ' . $user->getProfile()->getApellidos() .'"') ?>
-    <?php endif ?>
-  </h2>
-  <form action="#" method="post" accept-charset="utf-8">
-    <p><label for="body">¿Qué le decimos?</label></p>
-    <p><textarea id="body"></textarea></p>
-    <p class="submit"><input type="submit" value="Enviar" /></p>
-  </form>
-</div>
+<h2>
+  Mandar un mensaje a <a href="#"><?php echo $user ?></a>
+  <?php if($user->getProfile() && $user->getProfile()->getImagen() && $user->getProfile()->getImagen() != '' ): ?>
+    <?php echo image_tag('http://'.S3Voota::getBucketPub().'.s3.amazonaws.com/usuarios/cc_s_'.($user->getProfile()->getImagen()), 'alt="Foto '. $user->getProfile()->getNombre().' ' . $user->getProfile()->getApellidos() .'"') ?>
+  <?php endif ?>
+</h2>
+<form action="#" method="post" accept-charset="utf-8">
+  <p class="label"><label for="mensaje_cuerpo">¿Qué le decimos?</label></p>
+  <p class="textarea"><textarea id="mensaje_cuerpo" name="cuerpo" rows="8" cols="40"></textarea></p>
+  <p class="counter" id="cuerpo_counter"></p>
+  <p class="submit"><input type="submit" value="Enviar" /></p>
+</form>
