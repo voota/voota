88f13efe36fd2b0773c52b9804d5413169004790
Sergio Viteri <sergio@infovit.net>
2010-01-21 11:46:13 +0000

    git-svn-id: http://trac.voota.org/svn/voota/trunk@669 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- www/apps/frontend/modules/perfil/templates/showSuccess.php
+++ www/apps/frontend/modules/perfil/templates/showSuccess.php
@@ -3,6 +3,7 @@
 <?php use_helper('SfReview') ?>
 <?php use_helper('jQuery') ?>
 <?php use_helper('VoFormat') ?>
+<?php use_helper('Date') ?>
 
 <script type="text/javascript">
   <!--
@@ -65,20 +66,22 @@
       <div class="box-inner">
         <h3><?php echo __('Datos básicos') ?></h3>
         <p>
-          <?php echo __('En Voota desde el') ?> 23/06/2009
+          <?php echo __('En Voota desde el %1%', array('%1%' => format_date( $user->getCreatedAt() ))) ?>
+          <?php /* ?>
           <br />
           <?php echo __('Socio número') ?>: 4567V
           <br />
           <?php echo __('Función en Voota') ?>: Socio
           <a href="http://blog.voota.es/es/socios/">?</a>
+          <?php */ ?>
         </p>
         <h3><?php echo __('Donaciones') ?></h3>
-        <p><?php echo __('Voota se mantiene gracias a') ?> <a href="http://blog.voota.es/es/socios/">las donaciones</a></p>
-        <p><button>¿Hacer una donación?</button></p>
+        <p><?php echo __('Voota se mantiene gracias a <a href="http://blog.voota.es/es/socios/">las donaciones</a>') ?></p>
+        <p><button><?php echo __('¿Hacer una donación?') ?></button></p>
         <script type="text/javascript" charset="utf-8">
           $(document).ready(function() {
             $('#sidebar button').click(function() {
-              window.location = 'http://blog.voota.es/es/socios/';
+              window.location = 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=10999422';
             })
           })
         </script>
