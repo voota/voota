7952db0e59625b46ea302869bb1114e6c2346775
Sergio Viteri <sergio@infovit.net>
2010-01-28 17:33:34 +0000

    git-svn-id: http://trac.voota.org/svn/voota/trunk@708 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- www/apps/frontend/templates/layout.php
+++ www/apps/frontend/templates/layout.php
@@ -1,7 +1,7 @@
 <?php use_helper('I18N') ?>
 <?php use_helper('jQuery') ?>
 <?php use_helper('sfFacebookConnect'); ?>
-<?php use_helper('VVoUsev'); ?>
+<?php use_helper('VoUser'); ?>
 
 <?php echo "<?xml version='1.0' encoding='utf-8' ?>" ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
