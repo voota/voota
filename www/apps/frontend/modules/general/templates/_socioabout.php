702bd50d6ee56963b595dbb6d14a082b0efa7ffd
Carlos Paramio <carlos@evolve.st>
2010-02-01 18:23:09 +0000

    enlazar usuarios con sus páginas públicas en about
    git-svn-id: http://trac.voota.org/svn/voota/trunk@719 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- www/apps/frontend/modules/general/templates/_socioabout.php
+++ www/apps/frontend/modules/general/templates/_socioabout.php
@@ -4,7 +4,7 @@
   <div class="avatar">
 	  <?php echo image_tag('http://'.S3Voota::getBucketPub().'.s3.amazonaws.com/usuarios/cc_s_'.($socio->getProfile()->getImagen()), 'alt="'. __('Foto de %1%', array('%1%' => $socio->getProfile()->getNombre().' ' . $socio->getProfile()->getApellidos() .' (Voota)')) .'"') ?>
   </div>
-  <h4 class="name"><?php echo $socio->getProfile()->getNombre()?> <?php echo $socio->getProfile()->getApellidos()?> · <?php echo $socio->getProfile()->getPapelVoota()?></h4>
+  <h4 class="name"><?php echo link_to($socio, "@usuario?username=".$socio->getProfile()->getVanity()) ?> · <?php echo $socio->getProfile()->getPapelVoota()?></h4>
   <p class="body">
     <?php $presentacion = $socio->getProfile()->getPresentacion(); autolink($presentacion, false) ?>
     <?php echo $presentacion ?>
