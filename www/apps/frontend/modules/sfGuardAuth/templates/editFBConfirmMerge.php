b9c288afae081ff17d1db5750c20ae496ff914ed
Sergio Viteri <sergio@infovit.net>
2010-02-12 12:41:22 +0000

    git-svn-id: http://trac.voota.org/svn/voota/trunk@771 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- www/apps/frontend/modules/sfGuardAuth/templates/editFBConfirmMerge.php
+++ www/apps/frontend/modules/sfGuardAuth/templates/editFBConfirmMerge.php
@@ -5,7 +5,7 @@
     <strong><fb:name uid="<?php echo $faceBookUid ?>" useyou="false" linked="false"></fb:name></strong>
   </h3>
 <p>
-<?php echo __('Ojo tienes una cuenta en Voota y otra en Facebook. Unificando las dos cuentas todo será más sencillo. ¿Cómo lo ves?')?>
+<?php echo __('Ojo, ahora tienes otra cuenta asociada a tu perfil de Facebook. Unificando las dos todo será más sencillo. ¿Cómo lo ves?')?>
 </p>
 <form action="#">
 	<input onclick="return facebookConnect_loadPreferences('<?php echo url_for('@usuario_fb_confirm_merge') ?>');" type="submit" value="Ok, unificar cuentas">
