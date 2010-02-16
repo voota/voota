8de44daa19f5849e6e2d4c36cd7919dbef1c3f3b
Carlos Paramio <carlos@evolve.st>
2010-02-16 08:39:37 +0000

    tamaño de fuentes en px
    git-svn-id: http://trac.voota.org/svn/voota/trunk@787 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- www/apps/frontend/modules/general/templates/rulesSuccess.php
+++ www/apps/frontend/modules/general/templates/rulesSuccess.php
@@ -11,12 +11,12 @@
 
   <h3><?php echo __('¿Qué es lo que está prohibido por ley? (ojo, lista no exhaustiva)') ?></h3>
   <ul>
-    <li><h6><?php echo __('Publicar datos personales de terceros (políticos u otros usuarios)') ?></h6></li>
-    <li><h6><?php echo __('Insultar o amenazar.') ?></h6></li>
-    <li><h6><?php echo __('Incitar al odio, la violencia o la difamación.') ?></h6></li>
-    <li><h6><?php echo __('Incitar a la discriminación por razones de sexo, raza, religión, afiliación, política, creencias, edad o condición.') ?></h6></li>
-    <li><h6><?php echo __('Enlazar a páginas con contenidos ilegales.') ?></h6></li>
-    <li><h6><?php echo __('Incluir contenidos protegidos por derechos de propiedad de terceros sin autorización.') ?></h6></li>
+    <li><?php echo __('Publicar datos personales de terceros (políticos u otros usuarios)') ?></li>
+    <li><?php echo __('Insultar o amenazar.') ?></li>
+    <li><?php echo __('Incitar al odio, la violencia o la difamación.') ?></li>
+    <li><?php echo __('Incitar a la discriminación por razones de sexo, raza, religión, afiliación, política, creencias, edad o condición.') ?></li>
+    <li><?php echo __('Enlazar a páginas con contenidos ilegales.') ?></li>
+    <li><?php echo __('Incluir contenidos protegidos por derechos de propiedad de terceros sin autorización.') ?></li>
   </ul>
 
   <p>
