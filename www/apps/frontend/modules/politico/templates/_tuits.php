6c5e3cfa3a033423a293916b8f49a84844009166
Sergio Viteri <sergio@infovit.net>
2010-02-27 20:50:22 +0000

    git-svn-id: http://trac.voota.org/svn/voota/trunk@867 f81fdc05-8907-49aa-9672-817e67ad6f7d

--- www/apps/frontend/modules/politico/templates/_tuits.php
+++ www/apps/frontend/modules/politico/templates/_tuits.php
@@ -1,31 +1,8 @@
-<script src="http://widgets.twimg.com/j/2/widget.js"></script>
-<script>
-new TWTR.Widget({
-  version: 2,
-  type: 'profile',
-  rpp: 4,
-  interval: 6000,
-  width: 250,
-  height: 300,
-  theme: {
-    shell: {
-      background: '#ffffff',
-      color: '#000000'
-    },
-    tweets: {
-      background: '#ffffff',
-      color: '#000000',
-      links: '#0000ff'
-    }
-  },
-  features: {
-    scrollbar: false,
-    loop: false,
-    live: false,
-    hashtags: true,
-    timestamp: true,
-    avatars: false,
-    behavior: 'all'
-  }
-}).render().setUser('<?php echo $user?>').start();
-</script>
+<div>
+<div id="twitter_div">
+	<ul id="twitter_update_list"></ul>
+</div>
+<script src="http://twitter.com/javascripts/blogger.js" type="text/javascript"></script>
+<script type="text/javascript" src="http://twitter.com/statuses/user_timeline/username.json?callback=twitterCallback2&count=5"></script>
+
+</div>
