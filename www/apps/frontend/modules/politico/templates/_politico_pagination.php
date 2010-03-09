ecd113e79fd4d922a546aa898b7a04e876565bad
Carlos Paramio <carlosparamio@gmail.com>
2010-03-09 11:34:37 +0100

    partial paginador político

---
+++ www/apps/frontend/modules/politico/templates/_politico_pagination.php
@@ -0,0 +1,8 @@
+<?php if(true): // TODO: Si se viene del ranking general o del top5 de la home, pero no del buscador o de políticos más votados de la semana ?>
+  <p class="politico-pagination">
+    <a href="#">&laquo; Político anterior</a>
+    <span><strong>4</strong> de 133</span>
+    <a href="#">Político siguiente &raquo;</a>
+    <a class="back" href="#">Listado de políticos (PP, Madrid) por votos negativos</a>
+  </p>
+<?php endif ?>
