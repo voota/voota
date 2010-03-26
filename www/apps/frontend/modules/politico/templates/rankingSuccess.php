e395cb7dd3e4cfaceda895748dd8153eb14f466d
Sergio Viteri <sergio@infovit.net>
2010-03-26 18:21:19 +0100

    capa para decir "ejem" sin estar logged in

--- www/apps/frontend/modules/politico/templates/rankingSuccess.php
+++ www/apps/frontend/modules/politico/templates/rankingSuccess.php
@@ -23,6 +23,7 @@
 <?php include_partial('selectorPartido', array('pageTitle' => $pageTitle, 'partidos_arr' => $partidos_arr, 'favoritos' => $partidos_arr, 'partido' => $partido)) ?>
 
 <?php include_partial('global/institucionList', array('instituciones' => $instituciones, 'partido' => $partido, 'institucion' => $institucion, 'showPartido' => true)) ?>
+<?php include_partial('sfReviewFront/dialog', array('instituciones' => $instituciones, 'partido' => $partido, 'institucion' => $institucion, 'showPartido' => true)) ?>
 
 
 <table border="0" cellpadding="0" cellspacing="0">
@@ -83,9 +84,18 @@
           ) ?>
         </td>
         <td class="voto">
-          <div id="mv_1">
+          <div id="<?php echo "mv_up_".$politico->getId()?>">
+          <script>
+            $("#<?php echo "mv_up_".$politico->getId()?>").click(function(){
+              	$("#sfr_dialog").dialog('open');
+              	return false;
+            });          
+          </script>
+          <style>
+          	.ui-widget-content { border: 1px solid #3366ff; }
+          </style>
         	<span style="float:left; margin:0px 25px 0px 0px;">
-        	  <a href="#">
+        	  <a href="#" id="<?php echo "mv_up_$politico"?>">
         		<?php echo image_tag('icoMiniUp_dis.png', 'title="'. __('Votar a favor') .'" alt="'. __('yeah') .'"') ?>
         	  </a>
         	</span>
