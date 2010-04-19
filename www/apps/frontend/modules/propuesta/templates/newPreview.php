4fe459f842b06fc9e34a5c9962cd85258eec3acd
Carlos Paramio <carlosparamio@gmail.com>
2010-04-19 12:07:27 +0200

    anchos en propuesta-new

--- www/apps/frontend/modules/propuesta/templates/newPreview.php
+++ www/apps/frontend/modules/propuesta/templates/newPreview.php
@@ -4,45 +4,48 @@
 <?php use_helper('VoNotice') ?>
 
 <div class="propuesta-preview">
-
-  <h2><?php echo __('Así se verá: %nombre%', array('%nombre%' => $form['titulo']->getValue())) ?></h2>
-  <div class="picture">
-    <p><a href="#"><?php echo __('&laquo; Volver, hacer cambios')?></a></p>
-    <p><img src="/images/proto/img_propuesta.png" alt="Título de la propuesta" /><?php // TODO: Sustituir URL y texto alt ?></p>
-  </div>
-  <div id="external-links">
-    <?php if (true): // TODO: Si hay enlaces definidos?>
-      <h3><?php echo __('Enlaces externos')?></h3>
-      <ul>
-        <?php // foreach(enlace): ?>
-		      <li><a href="#">seisdeagosto.com/indica</a></li>
-		      <li><a href="#">seisdeagosto.com/indica</a></li>
-		      <li><a href="#">seisdeagosto.com/indica</a></li>
-		      <li><a href="#">seisdeagosto.com/indica</a></li>
-        <?php // endforeach ?>
-      </ul>
-    <?php endif ?>
-  </div>
-  <div class="description">
-    <h3><?php echo __('Sobre la propuesta') ?></h3>
-    <?php echo formatDesc($form['descripcion']->getValue()) ?>
-    <?php if (true): // TODO: Sustituir por 'si ha subido subido un fichero' ?>
-      <p>Documento: <a href="#">Normas_Generales.pdf</a> (245Mb)</p><?php // TODO: Sustituir por enlace a fichero y tamaño de fichero correcto?>
-    <?php endif ?>
+  <div class="inner">
+    <h2><?php echo __('Así se verá: %nombre%', array('%nombre%' => $form['titulo']->getValue())) ?></h2>
+    <div class="picture">
+      <p><a href="#"><?php echo __('&laquo; Volver, hacer cambios')?></a></p>
+      <p><img src="/images/proto/img_propuesta.png" alt="Título de la propuesta" /><?php // TODO: Sustituir URL y texto alt ?></p>
+    </div>
+    <div id="external-links">
+      <?php if (true): // TODO: Si hay enlaces definidos?>
+        <h3><?php echo __('Enlaces externos')?></h3>
+        <ul>
+          <?php // foreach(enlace): ?>
+  		      <li><a href="#">seisdeagosto.com/indica</a></li>
+  		      <li><a href="#">seisdeagosto.com/indica</a></li>
+  		      <li><a href="#">seisdeagosto.com/indica</a></li>
+  		      <li><a href="#">seisdeagosto.com/indica</a></li>
+          <?php // endforeach ?>
+        </ul>
+      <?php endif ?>
+    </div>
+    <div class="description">
+      <h3><?php echo __('Sobre la propuesta') ?></h3>
+      <?php echo formatDesc($form['descripcion']->getValue()) ?>
+      <?php if (true): // TODO: Sustituir por 'si ha subido subido un fichero' ?>
+        <p>Documento: <a href="#">Normas_Generales.pdf</a> (245Mb)</p><?php // TODO: Sustituir por enlace a fichero y tamaño de fichero correcto?>
+      <?php endif ?>
+    </div>
   </div>
 </div>
 
 <div class="vooto">
-  <h3><?php echo __('¿Todo en orden? ¿Seguimos?') ?></h3>
-  <p><strong><?php echo __('Importante')?></strong>: <?php echo __('una vez subida, no podrás borrar ni editar la propuesta. Sólo podrás hacer cambios sobre los enlaces o el fichero incluído.') ?></p>
-  <form> <?php // TODO: Sustituir por etiqueta de formulario apropiada ?>
-    <p class="facebook-only submit">
-	    <img src="/images/icoFacebook.png" width="16" height="16" alt="Facebook" />
-	    <label for="publicar_propuesta_en_facebook"><?php echo __('Publicar en mi Facebook') ?></label>
-	    <input type="checkbox" name="fb_publish" value="1" id="publicar_propuesta_en_facebook" />
-	  </p>
-	  <p class="submit"><input type="submit" value="<?php echo __('¡Ok! Subir propuesta') ?>" /></p>
-  </form>
+  <div class="inner">
+    <h3><?php echo __('¿Todo en orden? ¿Seguimos?') ?></h3>
+    <p><strong><?php echo __('Importante')?></strong>: <?php echo __('una vez subida, no podrás borrar ni editar la propuesta. Sólo podrás hacer cambios sobre los enlaces o el fichero incluído.') ?></p>
+    <form> <?php // TODO: Sustituir por etiqueta de formulario apropiada ?>
+      <p class="facebook-only submit">
+        <img src="/images/icoFacebook.png" width="16" height="16" alt="Facebook" />
+        <label for="publicar_propuesta_en_facebook"><?php echo __('Publicar en mi Facebook') ?></label>
+        <input type="checkbox" name="fb_publish" value="1" id="publicar_propuesta_en_facebook" />
+      </p>
+      <p class="submit"><input type="submit" value="<?php echo __('¡Ok! Subir propuesta') ?>" /></p>
+    </form>
+  </div>
 </div>
 
 <form method="post" action="<?php echo url_for('propuesta/new')?>">
--- www/apps/frontend/modules/propuesta/templates/newSuccess.php
+++ www/apps/frontend/modules/propuesta/templates/newSuccess.php
@@ -17,97 +17,99 @@
   //-->
 </script>
 
-<h2><?php echo __('Proponer una propuesta política:') ?></h2>
+<div class="new-success">
+  <h2><?php echo __('Proponer una propuesta política:') ?></h2>
 
-<?php echo showNotices( $sf_user ) ?>
+  <?php echo showNotices( $sf_user ) ?>
 
-<div id="content">
-  <div id="sidebar">
-    <?php include_partial('ultimasPropuestas') ?>
+  <div id="content">
+    <div id="sidebar">
+      <?php include_partial('ultimasPropuestas') ?>
+    </div>
+
+    <form method="post" action="<?php echo url_for('propuesta/new')?>" enctype="multipart/form-data">
+      <div><input type="hidden" name="op" value="r" /></div>
+      <table>
+        <tr>
+          <th><label for="propuesta_titulo"><?php echo __('Titular de la propuesta') ?></label></th>
+          <td>
+            <?php echo $form['titulo']->render() ?>
+            <?php echo $form['titulo']->renderError() ?>
+            <p id="propuesta_titulo_counter" class="counter"></p>
+          </td>
+          <td class="hints"><?php echo __('Necesario. Indica brevemende de qué va la propuesta política.') ?></td>
+        </tr>
+        <tr>
+          <th><label for="propuesta_descripcion"><?php echo __('Descripción') ?></label></th>
+          <td>
+            <?php echo $form['descripcion']->render() ?>
+            <?php echo $form['descripcion']->renderError() ?>
+            <p id="propuesta_descripcion_counter" class="counter"></p>
+          </td>
+          <td class="hints"><?php echo __('Necesario. Ofrece al resto de la comunidad una descripción de la propuesta. Recuerda que dispones de 600 caracteres.') ?></td>
+        </tr>
+        <tr>
+          <th><label for="propuesta_imagen"><?php echo __('Imagen') ?></label></th>
+          <td class="avatar">
+            <?php echo $form['imagen']->render() ?>
+            <?php echo $form['imagen']->renderError() ?>
+          </td>
+          <td class="hints"><?php echo __('Alguna imagen que sirva para ilustrar la propuesta (opcional, pero ayuda)') ?></td>
+        </tr>
+        <tr>
+          <th><label for="propuesta_documento"><?php echo __('Documento') ?></label></th>
+          <td>
+            <?php if (false): // TODO: Sustituir por 'si ya tiene subido un fichero' ?>
+              <p><a class="document" href="">Nombre_del_fichero.pdf</a> (233Mb)</p><?php //TODO: Sustituir enlace y tamaño del fichero ?>
+            <?php else: ?>
+              <p>PDF, Office, OpenOffice... (Máx 500Mb)</p>
+            <?php endif ?>
+            <?php echo $form['doc']->render() ?>
+            <?php echo $form['doc']->renderError() ?>
+          </td>
+          <td class="hints"><?php echo __('Incluye 1 fichero que ofrezca información adicional sobre la propuesta (opcional, pero ayuda)') ?></td>
+        </tr>
+        <tr>
+          <th><label for="propuesta_enlace_n1_url"><?php echo __('Enlaces')?></label></th>
+          <td>
+            <ol class="links">
+              <li>
+                <?php echo $form['enlace_n1']['orden']->render(array('value' => 1)) ?>
+                <?php echo $form['enlace_n1']['url']->render(array('title' => __('Ejemplo: Voota.es'))) ?>
+                <?php echo $form['enlace_n1']['url']->renderError() ?>
+              </li>
+              <li>
+                <?php echo $form['enlace_n2']['orden']->render(array('value' => 2)) ?>
+                <?php echo $form['enlace_n2']['url']->render() ?>
+                <?php echo $form['enlace_n2']['url']->renderError() ?>
+              </li>
+              <li>
+                <?php echo $form['enlace_n3']['orden']->render(array('value' => 3)) ?>
+                <?php echo $form['enlace_n3']['url']->render() ?>
+                <?php echo $form['enlace_n3']['url']->renderError() ?>
+              </li>
+              <li>
+                <?php echo $form['enlace_n4']['orden']->render(array('value' => 4)) ?>
+                <?php echo $form['enlace_n4']['url']->render() ?>
+                <?php echo $form['enlace_n4']['url']->renderError() ?>
+              </li>
+              <li>
+                <?php echo $form['enlace_n5']['orden']->render(array('value' => 5)) ?>
+                <?php echo $form['enlace_n5']['url']->render() ?>
+                <?php echo $form['enlace_n5']['url']->renderError() ?>
+              </li>
+            </ol>
+          </td>
+          <td class="hints"><?php echo __('(opcional, pero ayuda)') ?></td>
+        </tr>
+        <tr>
+          <th></th>
+          <td class="submit">
+            <span>¿Seguimos?</span>
+            <input name="button" type="submit" class="button" value="<?php echo __('Ver cómo quedará')?>" />
+          </td>
+        </tr>
+      </table>
+    </form>
   </div>
-  
-  <form method="post" action="<?php echo url_for('propuesta/new')?>" enctype="multipart/form-data">
-    <div><input type="hidden" name="op" value="r" /></div>
-    <table>
-      <tr>
-        <th><label for="propuesta_titulo"><?php echo __('Titular de la propuesta') ?></label></th>
-        <td>
-          <?php echo $form['titulo']->render() ?>
-          <?php echo $form['titulo']->renderError() ?>
-          <p id="propuesta_titulo_counter" class="counter"></p>
-        </td>
-        <td class="hints"><?php echo __('Necesario. Indica brevemende de qué va la propuesta política.') ?></td>
-      </tr>
-      <tr>
-        <th><label for="propuesta_descripcion"><?php echo __('Descripción') ?></label></th>
-        <td>
-          <?php echo $form['descripcion']->render() ?>
-          <?php echo $form['descripcion']->renderError() ?>
-          <p id="propuesta_descripcion_counter" class="counter"></p>
-        </td>
-        <td class="hints"><?php echo __('Necesario. Ofrece al resto de la comunidad una descripción de la propuesta. Recuerda que dispones de 600 caracteres.') ?></td>
-      </tr>
-      <tr>
-        <th><label for="propuesta_imagen"><?php echo __('Imagen') ?></label></th>
-        <td class="avatar">
-          <?php echo $form['imagen']->render() ?>
-          <?php echo $form['imagen']->renderError() ?>
-        </td>
-        <td class="hints"><?php echo __('Alguna imagen que sirva para ilustrar la propuesta (opcional, pero ayuda)') ?></td>
-      </tr>
-      <tr>
-        <th><label for="propuesta_documento"><?php echo __('Documento') ?></label></th>
-        <td>
-          <?php if (false): // TODO: Sustituir por 'si ya tiene subido un fichero' ?>
-            <p><a class="document" href="">Nombre_del_fichero.pdf</a> (233Mb)</p><?php //TODO: Sustituir enlace y tamaño del fichero ?>
-          <?php else: ?>
-            <p>PDF, Office, OpenOffice... (Máx 500Mb)</p>
-          <?php endif ?>
-          <?php echo $form['doc']->render() ?>
-          <?php echo $form['doc']->renderError() ?>
-        </td>
-        <td class="hints"><?php echo __('Incluye 1 fichero que ofrezca información adicional sobre la propuesta (opcional, pero ayuda)') ?></td>
-      </tr>
-      <tr>
-        <th><label for="propuesta_enlace_n1_url"><?php echo __('Enlaces')?></label></th>
-        <td>
-          <ol class="links">
-            <li>
-              <?php echo $form['enlace_n1']['orden']->render(array('value' => 1)) ?>
-              <?php echo $form['enlace_n1']['url']->render(array('title' => __('Ejemplo: Voota.es'))) ?>
-              <?php echo $form['enlace_n1']['url']->renderError() ?>
-            </li>
-            <li>
-              <?php echo $form['enlace_n2']['orden']->render(array('value' => 2)) ?>
-              <?php echo $form['enlace_n2']['url']->render() ?>
-              <?php echo $form['enlace_n2']['url']->renderError() ?>
-            </li>
-            <li>
-              <?php echo $form['enlace_n3']['orden']->render(array('value' => 3)) ?>
-              <?php echo $form['enlace_n3']['url']->render() ?>
-              <?php echo $form['enlace_n3']['url']->renderError() ?>
-            </li>
-            <li>
-              <?php echo $form['enlace_n4']['orden']->render(array('value' => 4)) ?>
-              <?php echo $form['enlace_n4']['url']->render() ?>
-              <?php echo $form['enlace_n4']['url']->renderError() ?>
-            </li>
-            <li>
-              <?php echo $form['enlace_n5']['orden']->render(array('value' => 5)) ?>
-              <?php echo $form['enlace_n5']['url']->render() ?>
-              <?php echo $form['enlace_n5']['url']->renderError() ?>
-            </li>
-          </ol>
-        </td>
-        <td class="hints"><?php echo __('(opcional, pero ayuda)') ?></td>
-      </tr>
-      <tr>
-        <th></th>
-        <td class="submit">
-          <span>¿Seguimos?</span>
-          <input name="button" type="submit" class="button" value="<?php echo __('Ver cómo quedará')?>" />
-        </td>
-      </tr>
-    </table>
-  </form>
 </div>
