b6a60b0bec33b3793df6381b372b075d8095e2b2
SergioViteri <sergio@infovit.net>
2010-05-03 09:37:37 +0200

    Ticket  #504

--- www/apps/backend/modules/propuesta/actions/actions.class.php
+++ www/apps/backend/modules/propuesta/actions/actions.class.php
@@ -13,4 +13,19 @@ require_once dirname(__FILE__).'/../lib/propuestaGeneratorHelper.class.php';
  */
 class propuestaActions extends autoPropuestaActions
 {
+	
+	
+	public function executeUpdate(sfWebRequest $request)
+  	{
+  		parent::executeUpdate( $request );
+  		if($this->form->getValue('imagen_delete') == "on"){
+			$this->form->getObject()->setImagen( null );
+		}
+  		if($this->form->getValue('doc_delete') == "on"){
+			$this->form->getObject()->setDoc( null );
+		}
+		$this->form->getObject()->save();
+  	}
+		  	
+	
 }
