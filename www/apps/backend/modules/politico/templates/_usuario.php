49ea0f927bc6b446aaf76679a310919afe4378d2
SergioViteri <sergio@infovit.net>
2010-05-04 09:25:56 +0200

    Fix para #513

--- www/apps/backend/modules/politico/templates/_usuario.php
+++ www/apps/backend/modules/politico/templates/_usuario.php
@@ -1,7 +1,7 @@
 <?php 
 if ($politico->getSfGuardUser()){
 ?>
-	<?php echo link_to($politico->getSfGuardUser(), "sfGuardUser/edit?id=".$politico->getSfGuardUser()->getId()) ?>
+	<?php echo link_to($politico->getSfGuardUser()->getProfile()->getNombreOri().' '.$politico->getSfGuardUser()->getProfile()->getApellidosOri(), "sfGuardUser/edit?id=".$politico->getSfGuardUser()->getId()) ?>
 <?php 
 }
 ?>
