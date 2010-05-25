5a1351508f0930c6cda20a78c16b6ecce1257858
SergioViteri <sergio@infovit.net>
2010-05-25 10:16:00 +0200

    Trabajando en concejales

--- metadata/munis.sql
+++ metadata/munis.sql
@@ -114,4 +114,41 @@ where c.geo_id = i.geo_id
 
 # Copia tabla de concejales
 # Borrar alcaldes repetidos
+select distinct count(*)
+from concejal c, politico_institucion pi
+where c.institucion_id = pi.institucion_id
+and c.posicion = 1
+and pi.cargo = 'AL';
+
+select nombre, apellidos, partido, concat(geo_id, "_", posicion) as pais, count(*) as num
+from concejal
+group by nombre, apellidos, partido , pais
+order by num desc
+
+select max(id) from politico;
+insert into politico (nombre, apellidos, partido_txt, pais) select nombre, apellidos, partido, concat(geo_id, "_", posicion) as pais  from concejal;
+
+insert into politico_i18n (id, culture, bio) 
+	select p.id, 'es', concat('Concejal de ', i.nombre,' 2007')
+	from concejal c, politico p, institucion_i18n i
+	where c.nombre = p.nombre and c.apellidos = p.apellidos and c.partido = p.partido_txt and concat(c.geo_id, "_", c.posicion) = p.pais
+	and i.id = c.institucion_id and i.culture = 'es';
+
+insert into politico_i18n (id, culture, bio) 
+	select p.id, 'ca', concat('Regidor de ', i.nombre,' 2007')
+	from concejal c, politico p, institucion_i18n i
+	where c.nombre = p.nombre and c.apellidos = p.apellidos and c.partido = p.partido_txt and concat(c.geo_id, "_", c.posicion) = p.pais
+	and i.id = c.institucion_id and i.culture = 'ca';
+
+insert into politico_institucion (politico_id, institucion_id, cargo)
+	select p.id, c.institucion_id, 'CJ'
+	from concejal c, politico p
+	where c.nombre = p.nombre and c.apellidos = p.apellidos and c.partido = p.partido_txt and concat(c.geo_id, "_", c.posicion) = p.pais
+	;
+
+update politico, partido
+set politico.partido_id = partido.id
+where politico.partido_txt = partido.nombre
+and politico.partido_id is null
+and politico.id > 10550;
 
