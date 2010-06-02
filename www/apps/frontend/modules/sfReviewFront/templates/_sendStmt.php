a085b56a18ce3e4b288dd139c03e958c75b0df9a
SergioViteri <sergio@infovit.net>
2010-06-02 09:40:10 +0200

    .

--- www/apps/frontend/modules/sfReviewFront/templates/_sendStmt.php
+++ www/apps/frontend/modules/sfReviewFront/templates/_sendStmt.php
@@ -7,7 +7,7 @@
 	<?php /* Otra opinion sobre un político */?>
 	<?php if($reviewType == null && isset($politico)): ?>
 	  var attachment = { 
-			'name': '<?php echo sq( __('Opiniones a favor y en contra de %2% en Voota', array('%2%' => sfVoUtil::secureString( $politico ))) )?><?php if ($politico->getPartido()):?> (<?php echo sq($politico->getPartido()) ?>)<?php endif ?>'
+			'name': '<?php echo sq( __('Opiniones a favor y en contra de %2% en Voota', array('%2%' => $politico)) )?><?php if ($politico->getPartido()):?> (<?php echo sq($politico->getPartido()) ?>)<?php endif ?>'
 			, 'href': '<?php echo url_for('politico/show?id='.$politico->getVanity(), true) ?>'
 	  		, 'media': [{ 
 	  			'type': 'image'
@@ -15,13 +15,13 @@
 	  			, 'href': '<?php echo url_for('politico/show?id='.$politico->getVanity(), true) ?>'
 	  		}] 
 	  };	  		
-	  var text_intro = this.v[0].checked?'<?php echo sq( __('vooto a favor de una opinión de un usuario sobre %1%', array('%1%' => sfVoUtil::secureString( $politico ))) )?>':'<?php echo sq( __('voota en contra de una opinión de un usuario sobre %1%', array('%1%' => sfVoUtil::secureString( $politico ))) )?>';
+	  var text_intro = this.v[0].checked?'<?php echo sq( __('vooto a favor de una opinión de un usuario sobre %1%', array('%1%' => $politico)) )?>':'<?php echo sq( __('voota en contra de una opinión de un usuario sobre %1%', array('%1%' => $politico)) )?>';
 	  text = text_intro + (text != ''?(': '+text):'');
 	  
 	<?php /* Otra opinion sobre un partido */?>
 	<?php elseif($reviewType == null && isset($partido)): ?>
 	  var attachment = { 
-			'name': '<?php echo __('Opiniones a favor y en contra del partido %2% en Voota', array('%2%' => sfVoUtil::secureString( $partido )))?>'
+			'name': '<?php echo sq( __('Opiniones a favor y en contra del partido %2% en Voota', array('%2%' => $partido)))?>'
 			, 'href': '<?php echo url_for('partido/show?id='.$partido->getAbreviatura(), true) ?>'
 	  		, 'media': [{ 
 	  			'type': 'image'
@@ -29,11 +29,11 @@
 	  			, 'href': '<?php echo url_for('partido/show?id='.$partido->getAbreviatura(), true) ?>'
 	  		}] 
 	  };
-	  var text_intro = this.v[0].checked?'<?php echo sq( __('vooto a favor de una opinión de un usuario sobre el partido %1%', array('%1%' => sfVoUtil::secureString( $partido ))) )?>':'<?php echo sq( __('voota en contra de una opinión de un usuario sobre %1%', array('%1%' => sfVoUtil::secureString( $partido ))) )?>';
+	  var text_intro = this.v[0].checked?'<?php echo sq( __('vooto a favor de una opinión de un usuario sobre el partido %1%', array('%1%' => $partido)) )?>':'<?php echo sq( __('voota en contra de una opinión de un usuario sobre %1%', array('%1%' => $partido)) )?>';
 	  text = text_intro + (text != ''?(': '+text):'');
 	<?php elseif($reviewType == null): ?>
 	  var attachment = { 
-			'name': '<?php echo __('Opiniones a favor y en contra de \"%2%\" en Voota', array('%2%' => sfVoUtil::secureString( $entity )))?>'
+			'name': '<?php echo sq( __('Opiniones a favor y en contra de \"%2%\" en Voota', array('%2%' => $entity)))?>'
 			, 'href': '<?php echo url_for($entity->getModule().'/show?id='.$entity->getVanity(), true) ?>'
 	  		, 'media': [{ 
 	  			'type': 'image'
@@ -41,13 +41,13 @@
 	  			, 'href': '<?php echo url_for($entity->getModule().'/show?id='.$entity->getVanity(), true) ?>'
 	  		}] 
 	  };
-	  var text_intro = this.v[0].checked?'<?php echo sq( __('vooto a favor de una opinión de un usuario sobre \"%1%\"', array('%1%' => sfVoUtil::secureString( $entity ))) )?>':'<?php echo sq( __('voota en contra de una opinión de un usuario sobre \"%1%\"', array('%1%' => sfVoUtil::secureString( $entity ))) )?>';
+	  var text_intro = this.v[0].checked?'<?php echo sq( __('vooto a favor de una opinión de un usuario sobre \"%1%\"', array('%1%' => $entity)) )?>':'<?php echo sq( __('voota en contra de una opinión de un usuario sobre \"%1%\"', array('%1%' => $entity)) )?>';
 	  text = text_intro + (text != ''?(': '+text):'');
 	  
 	<?php /* Sobre un politico */?>
 	<?php elseif($reviewType == Politico::NUM_ENTITY): ?>
 	  var attachment = { 
-			'name': '<?php echo sq( __('Opiniones a favor y en contra de %2% en Voota', array('%2%' => sfVoUtil::secureString( $politico ))) )?><?php if ($politico->getPartido()):?> (<?php echo sq( $politico->getPartido() ) ?>)<?php endif ?>'
+			'name': '<?php echo sq( __('Opiniones a favor y en contra de %2% en Voota', array('%2%' => $politico)) )?><?php if ($politico->getPartido()):?> (<?php echo sq( $politico->getPartido() ) ?>)<?php endif ?>'
 			, 'href': '<?php echo url_for('politico/show?id='.$politico->getVanity(), true) ?>'
 	  		, 'media': [{ 
 	  			'type': 'image'
@@ -55,13 +55,13 @@
 	  			, 'href': '<?php echo url_for('politico/show?id='.$politico->getVanity(), true) ?>'
 	  		}] 
 	  };	  		
-	  var text_intro = this.v[0].checked?'<?php echo __('vooto a favor de %1%', array('%1%' => sfVoUtil::secureString( $politico )))?>':'<?php echo __('voota en contra de %1%', array('%1%' => sfVoUtil::secureString( $politico )))?>';
+	  var text_intro = this.v[0].checked?'<?php echo __('vooto a favor de %1%', array('%1%' => $politico))?>':'<?php echo __('voota en contra de %1%', array('%1%' => $politico))?>';
 	  text = text_intro + (text != ''?(': '+text):'');
 	  
 	<?php /* Sobre un partido */?>
 	<?php elseif($reviewType == Partido::NUM_ENTITY): ?>
 	  var attachment = { 
-			'name': '<?php echo sq( __('Opiniones a favor y en contra del partido %2% en Voota', array('%2%' => sfVoUtil::secureString( $partido ))) )?>'
+			'name': '<?php echo sq( __('Opiniones a favor y en contra del partido %2% en Voota', array('%2%' => $partido)) )?>'
 			, 'href': '<?php echo url_for('partido/show?id='.$partido->getAbreviatura(), true) ?>'
 	  		, 'media': [{ 
 	  			'type': 'image'
@@ -69,11 +69,11 @@
 	  			, 'href': '<?php echo url_for('partido/show?id='.$partido->getAbreviatura(), true) ?>'
 	  		}] 
 	  };
-	  var text_intro = this.v[0].checked?'<?php echo sq( __('vooto a favor del partido %1%', array('%1%' => sfVoUtil::secureString( $partido ))) )?>':'<?php echo sq( __('voota en contra de %1%', array('%1%' => sfVoUtil::secureString( $partido ))) )?>';
+	  var text_intro = this.v[0].checked?'<?php echo sq( __('vooto a favor del partido %1%', array('%1%' => $partido)) )?>':'<?php echo sq( __('voota en contra de %1%', array('%1%' => $partido)) )?>';
 	  text = text_intro + (text != ''?(': '+text):'');	  
 	<?php else: ?>
 	  var attachment = { 
-			'name': '<?php echo sq( __('Opiniones a favor y en contra de %2% en Voota', array('%2%' => sfVoUtil::secureString( $entity ))) )?>'
+			'name': '<?php echo sq( __('Opiniones a favor y en contra de %2% en Voota', array('%2%' => $entity)) )?>'
 			, 'href': '<?php echo url_for($entity->getModule().'/show?id='.$entity->getVanity(), true) ?>'
 	  		, 'media': [{ 
 	  			'type': 'image'
@@ -81,7 +81,7 @@
 	  			, 'href': '<?php echo url_for($entity->getModule().'/show?id='.$entity->getVanity(), true) ?>'
 	  		}] 
 	  };
-	  var text_intro = this.v[0].checked?'<?php echo sq( __('vooto a favor de \"%1%\"', array('%1%' => sfVoUtil::secureString( $entity ))) )?>':'<?php echo sq( __('voota en contra de \"%1%\"', array('%1%' => sfVoUtil::secureString( $entity ))) )?>';
+	  var text_intro = this.v[0].checked?'<?php echo sq( __('vooto a favor de \"%1%\"', array('%1%' => $entity)) )?>':'<?php echo sq( __('voota en contra de \"%1%\"', array('%1%' => $entity)) )?>';
 	  text = text_intro + (text != ''?(': '+text):'');	
 	<?php endif ?>	
 
