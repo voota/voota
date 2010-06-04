e7145efdf24dd6ea88b84d033d8b064c24864faf
SergioViteri <sergio@infovit.net>
2010-06-04 13:06:35 +0200

    Helper para js de validaciones w3c

--- www/apps/frontend/modules/general/templates/_sparkline_box.php
+++ www/apps/frontend/modules/general/templates/_sparkline_box.php
@@ -1,9 +1,8 @@
-<span id="canvas_<?php echo $id ?>"></span>
-<script type="text/javascript" charset="utf-8">
-  var canvas = document.createElement("canvas");
-  canvas.setAttribute('class', 'sparkline');
-  canvas.setAttribute('id', '<?php echo $id ?>');
-  canvas.setAttribute('width', '100');
-  canvas.setAttribute('height', '25');
-  $('#canvas_<?php echo $id ?>').append(canvas);
-</script>
+<?php use_helper('VoSmartJS') ?>
+
+<?php echo jsWrite("canvas", array(
+	"class" => "sparkline"
+	, "width" => "100"
+	, "height" => "25"
+	, "id" => "$id"
+));?>
