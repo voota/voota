af577c1c012c76058526721901e2c8d95b662aab
Carlos Paramio <carlosparamio@gmail.com>
2010-06-04 11:31:32 +0200

    Usar jsWrite en canvas

--- www/apps/frontend/modules/general/templates/_sparkline_box.php
+++ www/apps/frontend/modules/general/templates/_sparkline_box.php
@@ -1,3 +1,1 @@
-<script type="text/javascript" charset="utf-8">
-  document.write('<canvas class="sparkline" id="<?php echo $id ?>" width="100" height="25"></canvas>');
-</script>
+<?php echo jsWrite('<canvas class="sparkline" id="'. $id . '" width="100" height="25"></canvas>') ?>
--- www/apps/frontend/modules/home/templates/_sparkline_box.php
+++ www/apps/frontend/modules/home/templates/_sparkline_box.php
@@ -1,3 +1,1 @@
-<script type="text/javascript" charset="utf-8">
-  document.write('<canvas id="<?php echo $id ?>" width="100" height="15"></canvas>');
-</script>
+<?php echo jsWrite('<canvas id="' . $id . '" width="100" height="15"></canvas>') ?>
