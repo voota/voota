73d3845ca19845ff5e28918ff36ba3a899ff640f
SergioViteri <sergio@infovit.net>
2010-06-07 14:37:28 +0200

    Paginador comun

--- www/plugins/sfReviewPlugin/modules/sfReviewFront/templates/_reviewListByUser.php
+++ www/plugins/sfReviewPlugin/modules/sfReviewFront/templates/_reviewListByUser.php
@@ -15,7 +15,7 @@
   });
 </script>
 	
-	<form id="filterForm" method="post" action="<?php echo url_for('perfil/show?username='.$user->getProfile()->getVanity());?>">
+	<form id="filterForm" method="post" action="<?php echo url_for( '@'.sfContext::getInstance()->getRouting()->getCurrentRouteName(). (isset($user)?'?username='.$user->getProfile()->getVanity():'') )?>">
   	  <p>
   	    <label for="filterForm_f"><?php echo __("Filtrar Vootos:") ?></label>
   	    <select id="filterForm_f" name="type_id">
