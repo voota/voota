29755c537f915faef9689a6a0c74ded655861f5e
Carlos Paramio <carlosparamio@gmail.com>
2010-07-01 12:39:18 +0200

    Pie de correos

---
+++ www/apps/frontend/templates/_mailFooter.php
@@ -0,0 +1,15 @@
+<?php echo __('
+  <p>&nbsp;</p>
+  <p>
+    Un abrazo,
+    <br />
+    Comunidad Voota
+  </p>
+  <p>--</p>
+  <p>
+    Nuestra web: <a href="http://voota.es/">http://voota.es</a><br />
+    Blog de Voota: <a href="http://blog.voota.es/">http://blog.voota.es</a><br />
+    Voota en Twitter: <a href="http://twitter.com/voota">http://twitter.com/voota</a><br />
+    Voota en Facebook: <a href="http://facebook.com/voota">http://facebook.com/voota</a><br />
+  </p>'
+) ?>
